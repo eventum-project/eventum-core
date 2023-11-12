492df508c78cd7c07795cf491f5878a90be14b7a
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-11-12 16:41:01 +0300

    Change run of `event studio`  to separate script

--- eventum/__main__.py
+++ eventum/__main__.py
@@ -2,46 +2,13 @@ import argparse
 
 
 def _initialize_argparser(argparser: argparse.ArgumentParser) -> None:
-    """Add subparsers and arguments for initial argparser object."""
+    """Add arguments for initial argparser object."""
 
-    subparsers = argparser.add_subparsers(
-        description='Use different subcomands to control eventum running action',
-        dest='subcommand',
-        required=True,
-        metavar='subcommand'
-    )
-
-    # eventum start
-    start_argparser = subparsers.add_parser(
-        'start',
-        help='start generating of events according to the provided configuration'
-    )
-
-    start_argparser.add_argument(
+    argparser.add_argument(
         '-c', '--config',
         required=True,
         help='Configuration file'
     )
-
-    # eventum studio
-    _ = subparsers.add_parser(
-        'studio',
-        help='run the eventum studio app',
-    )
-
-
-def handle_start_subcommand(args: argparse.Namespace) -> None:
-    """Execute corresponding actions for `start` subcommand."""
-
-    # TODO Implement
-    raise NotImplementedError
-
-
-def handle_studio_subcommand(args: argparse.Namespace) -> None:
-    """Execute corresponding actions for `studio` subcommand."""
-
-    # TODO Implement
-    raise NotImplementedError
 
 
 def main() -> None:
@@ -55,13 +22,10 @@ def main() -> None:
 
     _initialize_argparser(argparser)
 
-    args = argparser.parse_args()
+    _ = argparser.parse_args()
 
-    match args.subcommand:
-        case 'start':
-            handle_start_subcommand(args)
-        case 'studio':
-            handle_studio_subcommand(args)
+    # TODO Implement creating app object and starting it
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
