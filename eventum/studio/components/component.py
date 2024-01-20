9141b1fb6bac0cb7d2d4a4ae08f93091e81d73a4
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-01-20 13:21:35 +0300

    Update base component and key management mechanism

--- eventum/studio/components/component.py
+++ eventum/studio/components/component.py
@@ -1,11 +1,50 @@
 
-from typing import MutableMapping
+from typing import MutableMapping, Optional
 
-from eventum.studio.widget_key_management import (ContextualSessionState,
-                                                  WidgetKeyBuilder)
+import streamlit as st
+
+from eventum.studio.key_management import (ContextualSessionState,
+                                           WidgetKeysContext)
+
+from abc import ABC, abstractmethod
 
 
-class BaseComponent:
-    def __init__(self, session_state: MutableMapping) -> None:
-        self._wk = WidgetKeyBuilder(self.__class__.__name__)
+class BaseComponent(ABC):
+    """Base class for creating session isolated components."""
+
+    def __init__(
+        self,
+        session_state: MutableMapping = st.session_state,
+        id: int = 1,
+        widget_keys_context: Optional[WidgetKeysContext] = None
+    ) -> None:
+        if widget_keys_context is None:
+            self._wk = WidgetKeysContext()
+        else:
+            self._wk = widget_keys_context
+
+        self._wk.register_component(self.__class__.__name__, id)
+
         self._session_state = ContextualSessionState(session_state, self._wk)
+        self._init_session_wrapper()
+
+        self._show()
+
+    def _init_session_wrapper(self):
+        """Check whether the session is initilized and call
+        intialization in case it's not.
+        """
+        if 'initialized' in self._session_state:
+            return
+        self._init_session()
+        self._session_state['initialized'] = True
+
+    @abstractmethod
+    def _init_session(self) -> None:
+        """Perform session initialization."""
+        ...
+
+    @abstractmethod
+    def _show(self):
+        """Present widgets layout."""
+        ...
---
+++ eventum/studio/key_management.py
@@ -0,0 +1,49 @@
+from typing import Any, MutableMapping
+
+
+class WidgetKeysContext():
+    """Helper class for creating widget keys."""
+
+    _SEPARATOR = ':'
+    _ID_PREFIX = '-'
+
+    def __init__(
+        self,
+    ) -> None:
+        self._component_stack = []
+
+    def __call__(
+        self,
+        widget_key: str
+    ) -> str:
+        return WidgetKeysContext._SEPARATOR.join(
+            self._component_stack + [widget_key]
+        )
+
+    def register_component(self, component_name: str, component_id: int):
+        """Add component to context stack."""
+        self._component_stack.append(
+            f'{component_name}{WidgetKeysContext._ID_PREFIX}{component_id}'
+        )
+
+
+class ContextualSessionState:
+    """Wrapper class for streamlit session state that provides isolation
+    for widget keys from different contexts.
+    """
+    def __init__(
+        self,
+        st_session_state: MutableMapping,
+        widget_keys_context: WidgetKeysContext
+    ) -> None:
+        self._session_state = st_session_state
+        self._wk = widget_keys_context
+
+    def __getitem__(self, __key: Any) -> Any:
+        return self._session_state[self._wk(__key)]
+
+    def __setitem__(self, __key: Any, __value: Any) -> None:
+        self._session_state[self._wk(__key)] = __value
+
+    def __contains__(self, __key: object) -> bool:
+        return self._wk(__key) in self._session_state
