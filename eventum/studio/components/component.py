3eabc745e9f0727b21b0b7f6b6352b037e88c5b5
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-01-21 17:02:19 +0300

    Update base component

--- eventum/studio/components/component.py
+++ eventum/studio/components/component.py
@@ -31,28 +31,27 @@ class BaseComponent(ABC):
         self._wk.register_component(self.__class__.__name__, id)
         self._session_state = ContextualSessionState(session_state, self._wk)
 
-        self.__init_session_wrapper()
+        self.__init_state_wrapper()
         self._show()
 
-    def __init_session_wrapper(self):
+    def __init_state_wrapper(self):
         """Check whether the session is initilized and call
         intialization in case it's not.
         """
         if 'initialized' in self._session_state:
             return
-        self._init_session()
+        self._init_state()
         self._session_state['initialized'] = True
 
-    @abstractmethod
-    def _init_session(self) -> None:
-        """Perform session initialization."""
+    def _init_state(self) -> None:
+        """Perform state initialization."""
         ...
+
+    def _release_state(self) -> None:
+        """Delete items from session state added on initialization."""
+        del self._session_state['initialized']
 
     @abstractmethod
     def _show(self):
         """Present component structure."""
         ...
-
-    def _release_session(self) -> None:
-        """Delete items from session state added on initialization."""
-        del self._session_state['initialized']
