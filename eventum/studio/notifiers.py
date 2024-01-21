65991c58213d39a92160f916bdc84bf7d8b48463
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-01-21 13:14:17 +0300

    Add levels and colors for notifiers

--- eventum/studio/notifiers.py
+++ eventum/studio/notifiers.py
@@ -1,15 +1,50 @@
 import streamlit as st
+from enum import Enum
 
 
-def null_notifier(body, **kwargs):
+class NotificationLevel(Enum):
+    SUCCESS = 'success'
+    INFO = 'info'
+    WARNING = 'warning'
+    ERROR = 'error'
+
+
+class NotificationColor(Enum):
+    SUCCESS = 'green'
+    INFO = 'default'
+    WARNING = 'yellow'
+    ERROR = 'red'
+
+
+def null_notifier(
+    message: str,
+    level: NotificationLevel = NotificationLevel.INFO,
+):
     pass
 
 
-def logger_notifier(body, **kwargs):
+def logger_notifier(
+    message: str,
+    level: NotificationLevel = NotificationLevel.INFO,
+):
     raise NotImplementedError
 
 
-streamlit_toast_notifier = st.toast
+def streamlit_toast_notifier(
+    message: str,
+    level: NotificationLevel = NotificationLevel.INFO,
+):
+    match level:
+        case NotificationLevel.SUCCESS:
+            fmt_msg = f':{NotificationColor.SUCCESS.value}[{message}]'
+        case NotificationLevel.WARNING:
+            fmt_msg = f':{NotificationColor.WARNING.value}[{message}]'
+        case NotificationLevel.ERROR:
+            fmt_msg = f':{NotificationColor.ERROR.value}[{message}]'
+        case _:
+            fmt_msg = message
+
+    return lambda: st.toast(fmt_msg)
 
 
 default_notifier = streamlit_toast_notifier
