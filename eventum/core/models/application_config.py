bc24cf18c46c9f1cfea65ec12755c898931c41e6
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-02-24 19:15:40 +0300

    Add models for application config

--- eventum/core/models/application_config.py
+++ eventum/core/models/application_config.py
@@ -1,6 +1,116 @@
-from dataclasses import dataclass
+from enum import StrEnum
+from typing import Any, Dict, TypeAlias
+
+from pydantic import BaseModel, root_validator
 
 
-@dataclass
-class ApplicationConfig:
-    ...
+class InputType(StrEnum):
+    patterns = 'patterns'
+    timestamps = 'timestamps'
+    cron = 'cron'
+    sample = 'sample'
+
+
+PatternsInputConfig: TypeAlias = list[str]
+TimestampsInputConfig: TypeAlias = list[str]
+
+
+class CronInputConfig(BaseModel):
+    expression: str
+    count: int
+
+
+class SampleInputConfig(BaseModel):
+    count: int
+
+
+class SampleType(StrEnum):
+    csv = 'csv'
+    items = 'items'
+
+
+class CSVSampleConfig(BaseModel):
+    type: SampleType
+    header: bool | None = False
+    separator: str | None = ','
+    source: str
+
+
+class ItemsSampleConfig(BaseModel):
+    type: SampleType
+    source: list[str]
+
+
+class TemplatePickingMode(StrEnum):
+    all = 'all'
+    any = 'any'
+    chance = 'chance'
+    spin = 'spin'
+
+
+class TemplateCodecs(StrEnum):
+    json = 'json'
+    plain = 'plain'
+
+
+class TemplateConfig(BaseModel):
+    codec: TemplateCodecs
+    chance: int | None
+
+
+class SubrocessConfig(BaseModel):
+    config: str
+    detach: bool
+
+
+class EventConfig(BaseModel):
+    params: dict
+    samples: dict[str, ItemsSampleConfig | CSVSampleConfig]
+    mode: TemplatePickingMode
+    templates: dict[str, TemplateConfig]
+    subprocesses: dict[str, SubrocessConfig]
+
+    @root_validator
+    def validate_templates_parameters(cls, values: Dict[str, Any]):
+        for template in values['templates'].values():
+            if values['mode'] == TemplatePickingMode.chance:
+                if template.chance is None:
+                    raise ValueError(
+                        'Parameter "chance" must be set for specified template'
+                        ' picking mode'
+                    )
+            else:
+                if template.chance is not None:
+                    raise ValueError(
+                        'Parameter "chance" is not applicable for specified'
+                        ' template picking mode'
+                    )
+
+
+class OutputType(StrEnum):
+    stdout = 'stdout'
+    file = 'file'
+
+
+class StdOutOutputConfig(BaseModel):
+    pass
+
+
+class FileOutputConfig(BaseModel):
+    path: str
+
+
+InputConfigs: TypeAlias = (
+    PatternsInputConfig | TimestampsInputConfig
+    | SampleInputConfig | CronInputConfig
+)
+
+OutputConfigs: TypeAlias = (
+    StdOutOutputConfig | FileOutputConfig
+)
+
+
+class ApplicationConfig(BaseModel):
+    input: dict[InputType, InputConfigs]
+    event: EventConfig
+    output: dict[OutputType, OutputConfigs]
