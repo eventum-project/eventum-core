3439583d12174d9a9a0780b871656c6dcf348d57
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-02-24 14:34:50 +0300

    Add cli package

--- eventum/__main__.py
+++ eventum/__main__.py
@@ -1,31 +1,4 @@
-import argparse
-
-
-def _initialize_argparser(argparser: argparse.ArgumentParser) -> None:
-    """Add arguments for initial argparser object."""
-
-    argparser.add_argument(
-        '-c', '--config',
-        required=True,
-        help='Configuration file'
-    )
-
-
-def main() -> None:
-    argparser = argparse.ArgumentParser(
-        prog="eventum",
-        description="Flexible event generation tool",
-        epilog="Repo: https://github.com/rnv812/Eventum",
-    )
-
-    # TODO adjust logger
-
-    _initialize_argparser(argparser)
-
-    _ = argparser.parse_args()
-
-    # TODO Implement creating app object and starting it
-    raise NotImplementedError
+from eventum.cli.main import main
 
 
 if __name__ == '__main__':
---
+++ eventum/cli/main.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+def _initialize_argparser(argparser: argparse.ArgumentParser) -> None:
+    """Add arguments for initial argparser object."""
+
+    argparser.add_argument(
+        '-c', '--config',
+        required=True,
+        help='Configuration file'
+    )
+
+
+def main() -> None:
+    argparser = argparse.ArgumentParser(
+        prog="eventum",
+        description="Flexible event generation tool",
+        epilog="Repository: https://github.com/rnv812/Eventum",
+    )
+
+    # TODO adjust logger
+
+    _initialize_argparser(argparser)
+
+    _ = argparser.parse_args()
+
+    # TODO Implement creating app object and starting it
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    main()
