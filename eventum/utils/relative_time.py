233cbce5b7b10c793a7f37818a8bd16ee9d42930
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-02-25 09:57:10 +0300

    Update parse_relative_time logic

--- eventum/utils/relative_time.py
+++ eventum/utils/relative_time.py
@@ -6,33 +6,43 @@ def parse_relative_time(expression: str) -> timedelta:
     """Parse `expression` representing relative time and return
     coresponding`timedelta` object. The exression format is next:
 
-    <expression> ::= <term>{<term>}...
-    <term> ::= [<sign>]<value><unit>
+    <expression> ::= [<sign>]<term>{<term>}...
     <sign> ::= '+' | '-'
+    <term> ::= <value><unit>
     <value> ::= <integer>
     <unit> ::= 'd' | 'h' | 'm' | 's'
 
-    Example expressions: '+1d+12h'; '1h+30m+10s'; '-3d-4h'; '1d-2h+30m';
+    Example expressions: '+1d12h'; '1h30m10s'; '-3d4h'; '-1d2h30m';
     If expression cannot be properly parsed then `ValueError` is raised.
     """
     if not expression:
         raise ValueError('Empty expression is provided')
 
     pattern = (
-        r'(?:(?P<days>[-+]?\d+)d)?'
-        r'(?:(?P<hours>[-+]?\d+)h)?'
-        r'(?:(?P<minutes>[-+]?\d+)m)?'
-        r'(?:(?P<seconds>[-+]?\d+)s)?'
+        r'(?P<sign>[-+])?'
+        r'(?:(?P<days>\d+)d)?'
+        r'(?:(?P<hours>\d+)h)?'
+        r'(?:(?P<minutes>\d+)m)?'
+        r'(?:(?P<seconds>\d+)s)?'
     )
     match = re.match(pattern, expression)
 
     if match.start() != 0 or match.end() != (len(expression)):
         raise ValueError('Failed to parse expression')
 
+    groups = match.groupdict()
+    match groups.pop('sign'):
+        case None:
+            sign = 1
+        case '+':
+            sign = 1
+        case '-':
+            sign = -1
+
     return timedelta(
         **{
-            unit: int(value)
-            for unit, value in match.groupdict().items()
+            unit: int(value) * sign
+            for unit, value in groups.items()
             if value is not None
         }
     )
--- eventum/utils/tests/test_relative_time.py
+++ eventum/utils/tests/test_relative_time.py
@@ -10,26 +10,26 @@ def test_parse_relative_time():
     assert parse_relative_time('15m') == timedelta(minutes=15)
     assert parse_relative_time('30s') == timedelta(seconds=30)
     assert parse_relative_time('-1d') == timedelta(days=-1)
-    assert parse_relative_time('-15m-2s') == timedelta(minutes=-15, seconds=-2)
-    assert parse_relative_time('-1d+30s') == timedelta(days=-1, seconds=30)
-    assert parse_relative_time('12h-10m+15s') == timedelta(
-        hours=12, minutes=-10, seconds=15
+    assert parse_relative_time('-15m2s') == timedelta(minutes=-15, seconds=-2)
+    assert parse_relative_time('-1d30s') == timedelta(days=-1, seconds=-30)
+    assert parse_relative_time('-12h10m15s') == timedelta(
+        hours=-12, minutes=-10, seconds=-15
     )
-    assert parse_relative_time('50d+60h+70m+80s') == timedelta(
+    assert parse_relative_time('50d60h70m80s') == timedelta(
         days=50, hours=60, minutes=70, seconds=80
     )
-    assert parse_relative_time('-50d-60h-70m-80s') == timedelta(
+    assert parse_relative_time('-50d60h70m80s') == timedelta(
         days=-50, hours=-60, minutes=-70, seconds=-80
     )
-    assert parse_relative_time('+15d-15h+30m-24s') == timedelta(
-        days=15, hours=-15, minutes=30, seconds=-24
+    assert parse_relative_time('+15d15h30m24s') == timedelta(
+        days=15, hours=15, minutes=30, seconds=24
     )
 
     with pytest.raises(ValueError):
         parse_relative_time('16Days minus three hours')
 
     with pytest.raises(ValueError):
-        parse_relative_time('16d-3j')
+        parse_relative_time('16d3j')
 
     with pytest.raises(ValueError):
         parse_relative_time('')
