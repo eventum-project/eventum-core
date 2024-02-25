303aef454a71403ebecfff5acc28434a91dfd5c5
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-02-25 22:47:45 +0300

    Add live and sample method signatures for BaseTimeDistribution

--- eventum/core/time_distribution.py
+++ eventum/core/time_distribution.py
@@ -1,8 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Callable, NoReturn
 
 
 class BaseTimeDistribution(ABC):
-    ...
+    """Base class for all time distributions."""
+
+    @abstractmethod
+    def live(self, on_event: Callable[[str]]) -> None | NoReturn:
+        """Start simulation of time distribution in live. Every time
+        event is occured in distribution, the `on_triger` callable is
+        called with current timestamp as `str` in ISO-8601 format as a
+        single parameter. If distribution has no end time then function
+        execution never ends, otherwise `None` is returned in the end.
+        """
+        ...
+
+    @abstractmethod
+    def sample(self, on_event: Callable[[str]]) -> None:
+        """Start simulation of time distribution in sample mode. The
+        method behavior is similar to `live` method except for the fact
+        that execution is not tied to real time and there is no any delay
+        between `on_event` calls. Therefore distribution is expected to have
+        specific start and end time to generate finite sample of timestamps.
+        """
+        ...
 
 
 class CronTimeDistribution(BaseTimeDistribution):
