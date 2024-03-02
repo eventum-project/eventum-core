17a30ba821b210457a06dd36482e9664b71f6e1b
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-02 22:40:16 +0300

    Document existing settings

--- eventum/core/settings.py
+++ eventum/core/settings.py
@@ -1,6 +1,17 @@
-# Input parameters section
+# ========== Input parameters section ==========
+
+# Minimal time for input plugin to go sleep waiting for next event.
+# The higher the value, the more values from the future (equal to the
+# parameter value) will be generated in advance.
+# The influence of the parameter appears with an intense flow of events
+# (thousands or millions per second).
 SLEEP_MIN_SECONDS = 0.1
 
-# Output parameters section
+
+# ========== Output parameters section ==========
+
+# Minimal size of output queue with rendered events to perform flush.
 FLUSH_AFTER_SIZE = 100
+
+# Timeout after which output queue with rendered events is flushed.
 FLUSH_AFTER_SECONDS = 1.0
