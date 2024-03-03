1ca0adacafd8d0b7da41f911f8fe9d7747e26a3d
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-03 13:16:37 +0300

    Update core settings descriptions

--- eventum/core/settings.py
+++ eventum/core/settings.py
@@ -2,13 +2,14 @@
 
 # Minimal time for input plugin to go sleep waiting for next event.
 # The higher the value, the more values from the future (equal to the
-# parameter value) will be generated in advance.
+# parameter value) will be published in advance.
 # Сan be considered as batch size. E.g setting parameter to 10 seconds
 # will cause to publishing events every 10 seconds from the next 10
 # seconds interval. This parameter does not affect the timestamps
 # passed to callback at all.
+# Parameter is only actual in live mode and ignored for cron input
+# plugin.
 AHEAD_PUBLICATION_SECONDS = 0.1
-
 
 # ========== Output parameters section ==========
 
