0de4341f601cc955a7b17028113dbd8927f4d4aa
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-10 14:06:40 +0300

    Add event formatting in StdoutOutputPlugin

--- eventum/core/plugins/output/base.py
+++ eventum/core/plugins/output/base.py
@@ -1,15 +1,23 @@
 from abc import ABC, abstractmethod
 
 
+class OutputPluginError(Exception):
+    """Base exception for all output plugin errors."""
+
+
+class FormatError(OutputPluginError):
+    """Exception for formatting errors."""
+
+
 class BaseOutputPlugin(ABC):
     """Base class for all output plugins."""
 
     @abstractmethod
-    def write(self, content: str) -> None:
-        """Write single object to output stream."""
+    def write(self, event: str) -> None:
+        """Write single event to output stream."""
         ...
 
     @abstractmethod
-    def write_many(self, content: list[str]) -> None:
-        """Write many objects to output stream in optimized way."""
+    def write_many(self, events: list[str]) -> None:
+        """Write many events to output stream in optimized way."""
         ...
--- eventum/core/plugins/output/stdout.py
+++ eventum/core/plugins/output/stdout.py
@@ -1,13 +1,41 @@
+import json
 import sys
 
-from eventum.core.plugins.output.base import BaseOutputPlugin
+from eventum.core.models.application_config import OutputFormat
+from eventum.core.plugins.output.base import (BaseOutputPlugin, FormatError)
 
 
 class StdoutOutputPlugin(BaseOutputPlugin):
-    def write(self, content: str) -> None:
-        sys.stdout.write(content)
+    def __init__(self, format: OutputFormat) -> None:
+        self._format = format
+
+    def _format_event(self, event: str) -> str:
+        match self._format:
+            case OutputFormat.ORIGINAL:
+                return event
+            case OutputFormat.JSON_LINES:
+                try:
+                    return json.dumps(json.loads(event), ensure_ascii=False)
+                except Exception:
+                    raise FormatError('Failed to format event')
+
+    def write(self, event: str) -> None:
+        try:
+            fmt_content = self._format_event(event)
+        except FormatError:
+            return
+
+        sys.stdout.write(fmt_content)
         sys.stdout.flush()
 
-    def write_many(self, content: list[str]) -> None:
-        sys.stdout.writelines(content)
+    def write_many(self, events: list[str]) -> None:
+        fmt_events = []
+
+        for event in events:
+            try:
+                fmt_events.append(self._format_event(event))
+            except FormatError:
+                pass
+
+        sys.stdout.writelines(fmt_events)
         sys.stdout.flush()
