c8d25e08556b47e1c9ed8ad3d428a444047abf70
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-10 15:02:48 +0300

    Add newline appending in StdoutOutputPlugin

--- eventum/core/plugins/output/stdout.py
+++ eventum/core/plugins/output/stdout.py
@@ -1,5 +1,7 @@
 import json
+import os
 import sys
+from typing import assert_never
 
 from eventum.core.models.application_config import OutputFormat
 from eventum.core.plugins.output.base import (BaseOutputPlugin, FormatError)
@@ -18,6 +20,8 @@ class StdoutOutputPlugin(BaseOutputPlugin):
                     return json.dumps(json.loads(event), ensure_ascii=False)
                 except Exception:
                     raise FormatError('Failed to format event')
+            case val:
+                assert_never(val)
 
     def write(self, event: str) -> None:
         try:
@@ -26,6 +30,7 @@ class StdoutOutputPlugin(BaseOutputPlugin):
             return
 
         sys.stdout.write(fmt_content)
+        sys.stdout.write(os.linesep)
         sys.stdout.flush()
 
     def write_many(self, events: list[str]) -> None:
@@ -38,4 +43,5 @@ class StdoutOutputPlugin(BaseOutputPlugin):
                 pass
 
         sys.stdout.writelines(fmt_events)
+        sys.stdout.write(os.linesep)
         sys.stdout.flush()
