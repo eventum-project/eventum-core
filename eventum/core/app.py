390aed188ef14a488a96cfc59c1728d25765790e
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-13 23:28:43 +0300

    Add graceful handling of SIGINT and SIGTERM for all processes in application core

--- eventum/core/app.py
+++ eventum/core/app.py
@@ -1,8 +1,9 @@
 import logging
 import os
+import signal
 from multiprocessing import Event, Process, Queue
 from time import perf_counter, sleep
-from typing import NoReturn, assert_never
+from typing import Callable, NoReturn, assert_never
 
 import eventum.logging_config
 import psutil
@@ -32,6 +33,23 @@ eventum.logging_config.apply()
 logger = logging.getLogger(__name__)
 
 
+def _module_subprocess(module_name: str) -> Callable:
+    def decorator(f: Callable):
+        def wrapper(*args, **kwargs):
+            setproctitle(f'{getproctitle()} [{module_name}]')
+
+            signal.signal(
+                signal.SIGINT,
+                lambda signal, stack_frame: exit(0)
+            )
+
+            result = f(*args, **kwargs)
+            return result
+
+        return wrapper
+    return decorator
+
+
 class Application:
     def __init__(
         self,
@@ -52,15 +70,41 @@ class Application:
 
         self._is_input_done = Event()
 
+        self._proc_input = Process(
+            target=self._start_input_module,
+            args=(
+                self._config.input,
+                self._time_mode,
+                self._input_queue,
+                self._is_input_done
+            )
+        )
+        self._proc_event = Process(
+            target=self._start_event_module,
+            args=(
+                self._config.event,
+                self._input_queue,
+                self._output_queue,
+                self._is_input_queue_awaited
+            )
+        )
+        self._proc_output = Process(
+            target=self._start_output_module,
+            args=(
+                self._config.output,
+                self._output_queue,
+                self._is_output_queue_awaited
+            )
+        )
+
     @staticmethod
+    @_module_subprocess('input')
     def _start_input_module(
         input_conf: InputConfigMapping,
         time_mode: TimeMode,
         queue: Queue,
         is_done: Event  # type: ignore
     ) -> NoReturn:
-        setproctitle(f'{getproctitle()} [input]')
-
         input_type, input_conf = input_conf.popitem()
 
         match input_type:
@@ -105,14 +149,13 @@ class Application:
         is_done.set()
 
     @staticmethod
+    @_module_subprocess('event')
     def _start_event_module(
         config: JinjaEventConfig,
         input_queue: Queue,
         output_queue: Queue,
         is_incoming_awaited: Event  # type: ignore
     ) -> NoReturn:
-        setproctitle(f'{getproctitle()} [event]')
-
         event_plugin = JinjaEventPlugin(config)
 
         while is_incoming_awaited.is_set():
@@ -123,13 +166,12 @@ class Application:
             )
 
     @staticmethod
+    @_module_subprocess('output')
     def _start_output_module(
         config: OutputConfigMapping,
         queue: Queue,
         is_incoming_awaited: Event  # type: ignore
     ) -> NoReturn:
-        setproctitle(f'{getproctitle()} [output]')
-
         output_plugins: list[BaseOutputPlugin] = []
 
         for output, output_conf in config.items():
@@ -160,56 +202,36 @@ class Application:
                     plugin.write_many(events)
 
     def start(self) -> NoReturn:
+        self._proc_input.start()
+        self._proc_event.start()
+        self._proc_output.start()
 
-        _proc_input = Process(
-            target=self._start_input_module,
-            args=(
-                self._config.input,
-                self._time_mode,
-                self._input_queue,
-                self._is_input_done
-            )
-        )
-        _proc_event = Process(
-            target=self._start_event_module,
-            args=(
-                self._config.event,
-                self._input_queue,
-                self._output_queue,
-                self._is_input_queue_awaited
-            )
-        )
-        _proc_output = Process(
-            target=self._start_output_module,
-            args=(
-                self._config.output,
-                self._output_queue,
-                self._is_output_queue_awaited
-            )
-        )
+        logger.info('Application is started')
 
-        _proc_input.start()
-        _proc_event.start()
-        _proc_output.start()
-
-        setproctitle(f'{getproctitle()} [supervisor]')
-
+        setproctitle(f'{getproctitle()} [main]')
         _ = psutil.Process(os.getpid())
 
+        signal.signal(signal.SIGINT, self._handle_termination)
+        signal.signal(signal.SIGTERM, self._handle_termination)
+
+        exit_code = 0
+
         while True:
-            if not _proc_output.is_alive():
+            if not self._proc_output.is_alive():
                 logger.critical('Output subprocess terminated unexpectedly')
-                _proc_input.terminate()
-                _proc_event.terminate()
+                self._proc_input.terminate()
+                self._proc_event.terminate()
+                exit_code = 1
                 break
 
-            if not _proc_event.is_alive():
+            if not self._proc_event.is_alive():
                 logger.critical('Event subprocess terminated unexpectedly')
-                _proc_input.terminate()
-                _proc_output.terminate()
+                self._proc_input.terminate()
+                self._proc_output.terminate()
+                exit_code = 1
                 break
 
-            if not _proc_input.is_alive():
+            if not self._proc_input.is_alive():
                 if self._is_input_done.is_set():
                     logger.info('Input module completed successfully')
 
@@ -222,13 +244,25 @@ class Application:
                     while not self._output_queue.empty():
                         sleep(0.1)
                     self._is_output_queue_awaited.clear()
+
+                    exit_code = 0
                 else:
                     logger.critical('Input subprocess terminated unexpectedly')
-                    _proc_event.terminate()
-                    _proc_output.terminate()
+                    self._proc_event.terminate()
+                    self._proc_output.terminate()
+                    exit_code = 1
 
                 break
 
             sleep(0.1)
 
-        logger.info('Stopping application')
+        logger.info('Application is stopped')
+        exit(exit_code)
+
+    def _handle_termination(self, signal, stack_frame):
+        logger.info('Termination signal received')
+        self._proc_input.terminate()
+        self._proc_event.terminate()
+        self._proc_output.terminate()
+        logger.info('Application is stopped')
+        exit(0)
