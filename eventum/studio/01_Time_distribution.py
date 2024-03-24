755bfabcf422cd1f3d1ee0269c04ca94bd680efc
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-24 19:45:46 +0300

    Cache calculating of distributions

--- eventum/studio/01_Time_distribution.py
+++ eventum/studio/01_Time_distribution.py
@@ -1,12 +1,9 @@
-import streamlit as st
 import plotly.graph_objects as go
-
-from eventum.core.plugins.input.base import InputPluginRuntimeError
-from eventum.core.plugins.input.time_pattern import TimePatternInputPlugin
+import streamlit as st
+from eventum.studio.calculations import calculate_distribution
 from eventum.studio.components.component import persist_state
 from eventum.studio.components.time_pattern_adjusters_list import \
     TimePatternAdjustersList
-from eventum.studio.notifiers import NotificationLevel, default_notifier
 
 persist_state()
 
@@ -19,27 +16,16 @@ with st.sidebar:
     patterns_list.show()
 
 configs = patterns_list.get_pattern_configs()
-labels = patterns_list.get_pattern_labels()
 colors = patterns_list.get_pattern_colors()
 
-patterns = [TimePatternInputPlugin(config) for config in configs]
-
 fig = go.Figure()
-for pattern, label, color in zip(patterns, labels, colors):
-    data = []
-    try:
-        pattern.sample(lambda ts: data.append(ts))
-    except InputPluginRuntimeError as e:
-        default_notifier(
-            message=f'Pattern "{label}" is not visualized: {e}',
-            level=NotificationLevel.ERROR
-        )
+for config, color in zip(configs, colors):
     fig.add_trace(
         go.Histogram(
-            x=data,
-            name=label,
-            marker_color=color,
-            nbinsx=100
+            x=calculate_distribution(config),
+            name=config.label,
+            nbinsx=100,
+            marker_color=color
         )
     )
 
