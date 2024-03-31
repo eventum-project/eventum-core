4c081554fa66ff7f7229ca03f8029c6ce0688be7
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-03-31 21:34:50 +0300

    [#13] Merge bins count and downsampling to a single span parameter

--- eventum/studio/components/time_pattern/distribution_histogram.py
+++ eventum/studio/components/time_pattern/distribution_histogram.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from typing import Iterable
 
 import numpy as np
@@ -11,6 +12,8 @@ from eventum.core.plugins.input.base import InputPluginRuntimeError
 from eventum.core.plugins.input.time_pattern import TimePatternInputPlugin
 from eventum.studio.components.component import BaseComponent
 from eventum.studio.notifiers import NotificationLevel, default_notifier
+from eventum.utils.numpy_time import timedelta_to_seconds
+from eventum.utils.relative_time import parse_relative_time
 
 
 def _hash_config(config: TimePatternConfig) -> int:
@@ -51,44 +54,84 @@ def _calculate_sample(config: TimePatternConfig) -> NDArray[np.datetime64]:
 
 class DistributionHistogram(BaseComponent):
     """Component for visualizing time patterns distribution."""
+    _AUTO_SPAN_SAMPLE_SIZE = 100
 
     _SHOW_PROPS = {
         'configs': Iterable[TimePatternConfig],
         'colors': Iterable[str],
-        'bins_count': int,
-        'downsampling': bool,
-        'downsampling_span': str
+        'use_custom_span': bool,
+        'span_expression': str
     }
+
+    def _resample_series(self, series: pd.Series) -> pd.Series:
+        """Resample series corresponding to specified span options. If
+        auto span is used, then sample is resampled according to
+        `_AUTO_SPAN_SAMPLE_SIZE`, otherwise `span_expression` prop is
+        used.
+        """
+        use_custom_span: bool = self._props['use_custom_span']
+        span_expression: str = self._props['span_expression']
+
+        if use_custom_span:
+            custom_span = parse_relative_time(span_expression)
+            return series.resample(rule=custom_span).sum()
+        else:
+            auto_span = timedelta(
+                seconds=timedelta_to_seconds(
+                    (series[-1] - series[0]) / self._AUTO_SPAN_SAMPLE_SIZE
+                )
+            )
+            return series.resample(rule=auto_span).sum()
+
+    def _calculate_bins_count(self, timedelta: np.timedelta64) -> int:
+        """Calculate count of bins in histogram for sample with
+        specified `timedelta` range (from start to end) according to
+        span configuration.
+        """
+        use_custom_span: bool = self._props['use_custom_span']
+        span_expression: str = self._props['span_expression']
+
+        if use_custom_span:
+            return int(
+                np.ceil(timedelta / parse_relative_time(span_expression))
+            )
+        else:
+            return self._AUTO_SPAN_SAMPLE_SIZE
 
     def _show(self) -> None:
         configs: Iterable[TimePatternConfig] = self._props['configs']
-        bins_count: int = self._props['bins_count']
         colors: Iterable[str] = self._props['colors']
-        downsampling: bool = self._props['downsampling']
-        downsampling_span: str = self._props['downsampling_span']
 
-        series: list[pd.Series] = []
+        series_list: list[pd.Series] = []
         labels: list[str] = []
         total_events = 0
 
+        min_timestamp = np.datetime64('9999-12-31')
+        max_timestamp = np.datetime64('0000-01-01')
+
         for config in configs:
-            sample = _calculate_sample(config)
-            ser = pd.Series(1, index=sample)
-            total_events += ser.size
+            series = pd.Series(1, index=_calculate_sample(config))
+            total_events += series.size
 
-            if downsampling:
-                ser = ser.resample(rule=downsampling_span).sum()
+            series = self._resample_series(series)
 
-            series.append(ser)
+            min_timestamp = min(min_timestamp, series[0])
+            max_timestamp = max(max_timestamp, series[-1])
+
+            series_list.append(series)
             labels.append(config.label)
+
+        bins_count = self._calculate_bins_count(
+            timedelta=(max_timestamp - min_timestamp)
+        )
 
         fig = go.Figure()
 
-        for ser, label, color in zip(series, labels, colors):
+        for series, label, color in zip(series_list, labels, colors):
             fig.add_trace(
                 go.Histogram(
-                    x=ser.index,
-                    y=ser.values,
+                    x=series.index,
+                    y=series.values,
                     histfunc='sum',
                     name=label,
                     nbinsx=bins_count,
