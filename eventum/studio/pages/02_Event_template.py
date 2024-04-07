071f0dde40c731cb16285666e67a83d37c37b40f
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-04-07 20:20:33 +0300

    [#11] Add monaco editor to Event template page

--- eventum/studio/pages/02_Event_template.py
+++ eventum/studio/pages/02_Event_template.py
@@ -3,6 +3,9 @@ from eventum.studio.components.component import persist_state
 from eventum.studio.components.template_configurator import \
     TemplateConfigurator
 
+from streamlit_elements import elements, editor
+
+
 persist_state()
 
 st.set_page_config(
@@ -11,14 +14,32 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
+if 'template_content' not in st.session_state:
+    st.session_state['template_content'] = ''
+
 configs_list = TemplateConfigurator(
     props={
-        'get_content_callback': lambda: st.session_state['test'],
-        'set_content_callback': lambda content: st.session_state.__setitem__('test', content)
+        'get_content_callback': (
+            lambda: st.session_state['template_content']
+        ),
+        'set_content_callback': (
+            lambda content:
+            st.session_state.__setitem__('template_content', content)
+        )
     }
 )
 with st.sidebar:
     configs_list.show()
 
 
-st.text_area(label='Template content', key='test')
+with elements('editor'):
+    editor.Monaco(
+        theme='vs-dark',
+        language='javascript',
+        value=st.session_state['template_content'],
+        onChange=(
+            lambda content:
+            st.session_state.__setitem__('template_content', content)
+        ),
+        height=580,
+    )
