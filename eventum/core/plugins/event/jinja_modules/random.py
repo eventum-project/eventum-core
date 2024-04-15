1886a6bee8f0c801c9be64c36ac89d12d7d75226
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-04-15 22:17:27 +0300

    [#5] Add functions for generating random numbers

--- eventum/core/plugins/event/jinja_modules/random.py
+++ eventum/core/plugins/event/jinja_modules/random.py
@@ -1,2 +1,34 @@
 import random
 
+
+# categories = [
+#     'number',
+#     'string',
+#     'geo',
+#     'system',
+#     'software',
+#     'network',
+#     'web',
+#     'crypto',
+# ]
+
+
+class number:
+    """Namespace for generating random numbers."""
+
+    @staticmethod
+    def integer(a: int, b: int) -> int:
+        """Return random integer in range [a, b]."""
+        return random.randint(a, b)
+
+    @staticmethod
+    def floating(a: float, b: float) -> float:
+        """Return random floating point number in range [a, b]."""
+        return random.uniform(a, b)
+
+    @staticmethod
+    def gauss(mu: float, sigma: float) -> float:
+        """Return random floating point number from Gaussian
+        distribution.
+        """
+        return random.gauss(mu, sigma)
