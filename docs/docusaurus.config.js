db26877fb5aaa6a289be5a92e1d711615cc53344
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-04-26 21:10:41 +0300

    Update docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://rnv812.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Eventum/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rnv812', // Usually your GitHub org/user name.
+  projectName: 'Eventum', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
