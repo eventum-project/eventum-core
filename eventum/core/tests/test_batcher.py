1b5728f08ec33a1ee21c1e0926ef56fbf85d27c4
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-04-28 08:38:21 +0300

    [#2] Add tests for batcher

---
+++ eventum/core/tests/test_batcher.py
@@ -0,0 +1,26 @@
+import time
+
+from eventum.core.batcher import Batcher
+
+
+def test_batcher_size_condition():
+    bucket = []
+    with Batcher(size=10, timeout=0.1, callback=bucket.append) as batcher:
+        for i in range(100):
+            batcher.add(i)
+
+    assert len(bucket) == 10
+    for batch in bucket:
+        assert len(batch) == 10
+
+
+def test_batcher_timeout_condition():
+    bucket = []
+    with Batcher(size=10, timeout=0.01, callback=bucket.append) as batcher:
+        for i in range(10):
+            batcher.add(i)
+            time.sleep(0.05)
+
+    assert len(bucket) == 10
+    for batch in bucket:
+        assert len(batch) == 1
