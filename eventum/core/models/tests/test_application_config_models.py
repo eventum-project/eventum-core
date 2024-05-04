98c7bfd11b08937512fdf985667ac3381a0fc452
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-04 17:54:30 +0300

    [#24] Update tests for application config models

--- eventum/core/models/tests/test_application_config_models.py
+++ eventum/core/models/tests/test_application_config_models.py
@@ -22,8 +22,34 @@ def test_crontab_input_config():
     CronInputConfig(expression='* * * * *', count=5)
 
 
+def test_crontab_input_config_with_empty_expression():
+    with pytest.raises(ValidationError):
+        CronInputConfig(expression='', count=1)
+
+
+def test_crontab_input_config_with_invalid_expression():
+    with pytest.raises(ValidationError):
+        CronInputConfig(expression='* * % * *', count=1)
+
+
+def test_crontab_input_config_with_invalid_count():
+    with pytest.raises(ValidationError):
+        CronInputConfig(expression='* * * * *', count=0)
+
+    with pytest.raises(ValidationError):
+        CronInputConfig(expression='* * * * *', count=-10)
+
+
 def test_sample_input_config():
     SampleInputConfig(count=100)
+
+
+def test_sample_input_config_with_invalid_count():
+    with pytest.raises(ValidationError):
+        SampleInputConfig(count=0)
+
+    with pytest.raises(ValidationError):
+        SampleInputConfig(count=-10)
 
 
 def test_csv_sample_config():
@@ -38,10 +64,37 @@ def test_csv_sample_config():
         source='test.csv'
     )
 
+
+def test_csv_sample_config_with_empty_delimiter():
+    with pytest.raises(ValidationError):
+        CSVSampleConfig(
+            type=SampleType.CSV,
+            delimiter='',
+            source='test.csv'
+        )
+
+
+def test_csv_sample_config_with_empty_source():
     with pytest.raises(ValidationError):
         CSVSampleConfig(
             type=SampleType.CSV,
             source=''
+        )
+
+
+def test_csv_sample_config_with_invalid_source():
+    with pytest.raises(ValidationError):
+        CSVSampleConfig(
+            type=SampleType.CSV,
+            source='test.xlsx'
+        )
+
+
+def test_csv_sample_config_with_invalid_type():
+    with pytest.raises(ValidationError):
+        CSVSampleConfig(
+            type=SampleType.ITEMS,
+            source='test.csv'
         )
 
 
@@ -50,6 +103,9 @@ def test_items_sample_config():
         type=SampleType.ITEMS,
         source=['item1', 'item2', 'item3']
     )
+
+
+def test_items_sample_config_with_empty_source():
     with pytest.raises(ValidationError):
         ItemsSampleConfig(
             type=SampleType.ITEMS,
@@ -57,9 +113,30 @@ def test_items_sample_config():
         )
 
 
+def test_items_sample_config_with_invalid_type():
+    with pytest.raises(ValidationError):
+        ItemsSampleConfig(
+            type=SampleType.CSV,
+            source=['item1', 'item2', 'item3']
+        )
+
+
 def test_template_config():
-    TemplateConfig(template='tests/test.json.jinja')
-    TemplateConfig(template='tests/test.json.jinja', chance=0.8)
+    TemplateConfig(template='test.json.jinja')
+    TemplateConfig(template='test.json.jinja', chance=0.8)
+
+
+def test_template_config_with_invalid_template():
+    with pytest.raises(ValidationError):
+        TemplateConfig(template='test', chance=0)
+
+
+def test_template_config_with_invalid_chance():
+    with pytest.raises(ValidationError):
+        TemplateConfig(template='test.json.jinja', chance=0)
+
+    with pytest.raises(ValidationError):
+        TemplateConfig(template='test.json.jinja', chance=-10)
 
 
 def test_jinja_event_config():
@@ -77,11 +154,31 @@ def test_jinja_event_config():
         },
         mode=TemplatePickingMode.ALL,
         templates={
-            'test_template': TemplateConfig(
-                template='tests/test.json.csv'
+            'test': TemplateConfig(
+                template='test.json.jinja'
             )
         }
     )
+    JinjaEventConfig(
+        params={},
+        samples={},
+        mode=TemplatePickingMode.ALL,
+        templates={
+            'test_template': TemplateConfig(
+                template='test.json.jinja'
+            )
+        }
+    )
+
+
+def test_jinja_event_config_with_empty_templates():
+    with pytest.raises(ValidationError):
+        JinjaEventConfig(
+            params={},
+            samples={},
+            mode=TemplatePickingMode.ALL,
+            templates={}
+        )
 
 
 def test_std_out_output_config():
@@ -94,11 +191,18 @@ def test_null_output_config():
 
 
 def test_file_output_config():
-    FileOutputConfig(path='test.out')
-    FileOutputConfig(path='test.out', format=OutputFormat.JSON_LINES)
+    FileOutputConfig(path='/tmp/test.out')
+    FileOutputConfig(path='/tmp/test.out', format=OutputFormat.JSON_LINES)
 
+
+def test_file_output_config_with_empty_path():
     with pytest.raises(ValidationError):
         FileOutputConfig(path='')
+
+
+def test_file_output_config_with_relative_path():
+    with pytest.raises(ValidationError):
+        FileOutputConfig(path='../relative.txt')
 
 
 def test_opensearch_output_config():
@@ -116,6 +220,8 @@ def test_opensearch_output_config():
         verify_ssl=False
     )
 
+
+def test_opensearch_output_config_with_invalid_hosts():
     with pytest.raises(ValidationError):
         OpensearchOutputConfig(
             hosts=[],
@@ -124,6 +230,8 @@ def test_opensearch_output_config():
             verify_ssl=False
         )
 
+
+def test_opensearch_output_config_with_invalid_user():
     with pytest.raises(ValidationError):
         OpensearchOutputConfig(
             hosts=['https://localhost:9200'],
@@ -132,6 +240,8 @@ def test_opensearch_output_config():
             verify_ssl=False
         )
 
+
+def test_opensearch_output_config_with_invalid_index():
     with pytest.raises(ValidationError):
         OpensearchOutputConfig(
             hosts=['https://localhost:9200'],
@@ -142,31 +252,35 @@ def test_opensearch_output_config():
 
 
 def test_application_config():
-    event_config = JinjaEventConfig(
-        params={'param': 'value'},
-        samples={
-            'csv_sample': CSVSampleConfig(
-                type=SampleType.CSV,
-                source='test.csv'
-            ),
-            'items_sample': ItemsSampleConfig(
-                type=SampleType.ITEMS,
-                source=['item1', 'item2', 'item3']
-            )
-        },
-        mode=TemplatePickingMode.ALL,
-        templates={
-            'test_template': TemplateConfig(
-                template='tests/test.json.csv'
-            )
-        }
-    )
     ApplicationConfig(
         input={InputName.SAMPLE: SampleInputConfig(count=100)},
-        event=event_config,
+        event=JinjaEventConfig(
+            params={},
+            samples={},
+            mode=TemplatePickingMode.ALL,
+            templates={'test': TemplateConfig(template='test.json.jinja')}
+        ),
         output={OutputName.NULL: NullOutputConfig()}
     )
 
+
+def test_application_config_with_plural_output():
+    ApplicationConfig(
+        input={InputName.SAMPLE: SampleInputConfig(count=100)},
+        event=JinjaEventConfig(
+            params={},
+            samples={},
+            mode=TemplatePickingMode.ALL,
+            templates={'test': TemplateConfig(template='test.json.jinja')}
+        ),
+        output={
+            OutputName.NULL: NullOutputConfig(),
+            OutputName.STDOUT: StdOutOutputConfig()
+        }
+    )
+
+
+def test_application_config_with_plural_input():
     with pytest.raises(ValidationError):
         ApplicationConfig(
             input={
@@ -176,20 +290,39 @@ def test_application_config():
                     count=1
                 )
             },
-            event=event_config,
+            event=JinjaEventConfig(
+                params={},
+                samples={},
+                mode=TemplatePickingMode.ALL,
+                templates={'test': TemplateConfig(template='test.json.jinja')}
+            ),
             output={OutputName.NULL: NullOutputConfig()}
         )
 
-    with pytest.raises(ValidationError):
-        ApplicationConfig(
-            input={InputName.SAMPLE: SampleInputConfig(count=100)},
-            event=event_config,
-            output={}
-        )
 
+def test_application_config_with_empty_input():
     with pytest.raises(ValidationError):
         ApplicationConfig(
             input={},
-            event=event_config,
+            event=JinjaEventConfig(
+                params={},
+                samples={},
+                mode=TemplatePickingMode.ALL,
+                templates={'test': TemplateConfig(template='test.json.jinja')}
+            ),
             output={OutputName.NULL: NullOutputConfig()}
+        )
+
+
+def test_application_config_with_empty_output():
+    with pytest.raises(ValidationError):
+        ApplicationConfig(
+            input={InputName.SAMPLE: SampleInputConfig(count=100)},
+            event=JinjaEventConfig(
+                params={},
+                samples={},
+                mode=TemplatePickingMode.ALL,
+                templates={'test': TemplateConfig(template='test.json.jinja')}
+            ),
+            output={}
         )
