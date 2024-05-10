3f9b349b231f05a04fa360958ce9d9015acb6c05
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-10 13:18:45 +0300

    [#6] Update docusaurus configs

---
+++ docs/blog/2024-05-10-welcome/index.md
@@ -0,0 +1,8 @@
+---
+slug: welcome
+title: Welcome
+authors: [rnv812]
+tags: [welcome]
+---
+
+Welcome to Eventum Blog! News about Eventum will be posted here.
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,4 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
+// @ts-nocheck
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
@@ -12,24 +8,16 @@ const config = {
   tagline: 'Flexible event generator',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://rnv812.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Eventum/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'rnv812', // Usually your GitHub org/user name.
-  projectName: 'Eventum', // Usually your repo name.
+  organizationName: 'rnv812',
+  projectName: 'Eventum',
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -42,17 +30,9 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -64,79 +44,60 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Eventum',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Eventum Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'gettingStartedSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Getting started',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/rnv812/Eventum',
             label: 'GitHub',
             position: 'right',
+            href: 'https://github.com/rnv812/Eventum',
+          },
+          {
+            to: '/blog',
+            label: 'Blog',
+            position: 'right'
           },
         ],
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        links: [],
+        copyright: `© Eventum, ${new Date().getFullYear()} `,
       },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
     }),
+    themes: [
+      [
+        require.resolve('@easyops-cn/docusaurus-search-local'),
+        /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+        ({
+          hashed: true,
+          indexPages: true,
+          searchBarShortcut: false,
+          removeDefaultStemmer: true,
+          highlightSearchTermsOnTargetPage: true
+        })
+      ]
+    ]
 };
 
 export default config;
