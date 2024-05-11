6487810f2a88026b0b773bc6ea4a34da3b30e601
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-11 22:37:20 +0300

    [#6] Fix link in get started button

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/overview/">
+            to="/docs/getting_started/overview/">
             Get started
           </Link>
         </div>
