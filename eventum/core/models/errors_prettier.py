72ceb76ce7e4e348d1c15cf64d7bf5d6294d6c23
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-14 22:32:02 +0300

    [#25] Fix issue in errors prettier

--- eventum/core/models/errors_prettier.py
+++ eventum/core/models/errors_prettier.py
@@ -1,12 +1,22 @@
+from typing import Iterable
+
+
 def prettify_errors(errors: list[dict]) -> str:
+    """Format pydantic dictionary gotten from `e.errors()` to pretty
+    human readable string."""
+
+    def _loc(location: Iterable[str | int]) -> str:
+        """Format field location."""
+        return '.'.join(map(str, location))
+
     messages: list[str] = []
 
     for error in errors:
         match error:
             case {'type': 'extra_forbidden', 'loc': loc}:
                 msg = 'Field is unrecognized'
-                messages.append(f'Field \"{".".join(loc)}\" - {msg}')
+                messages.append(f'Field \"{_loc(loc)}\" - {msg}')
             case {'msg': msg, 'loc': loc}:
-                messages.append(f'Field \"{".".join(loc)}\" - {msg}')
+                messages.append(f'Field \"{_loc(loc)}\" - {msg}')
 
     return '; '.join(messages)
