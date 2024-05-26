4f33032c240329b616bcaa3825ce1feeeb79d56f
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-26 23:16:54 +0300

    Add tests for batcher and creds manager

---
+++ eventum_core/tests/test_credentials_manager.py
@@ -0,0 +1,12 @@
+import os
+
+import pytest
+
+from eventum_core.credentials_manager import get_credentials_manager
+
+
+def test_non_interactive_password():
+    os.environ['KEYRING_CRYPTFILE_PASSWORD'] = 'incorrect pass'
+
+    with pytest.raises(ValueError):
+        get_credentials_manager()
