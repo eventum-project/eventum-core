1a5a43c4f59d94de6e141baf041ace00991030c4
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-07-11 21:16:28 +0300

    Add eventum-keyring cli

--- eventum_cli/config_finalizer.py
+++ eventum_cli/config_finalizer.py
@@ -1,56 +1,17 @@
 import json
-import os
 import re
 from collections import defaultdict
 from enum import StrEnum
-from functools import lru_cache
 from typing import Any, assert_never
 
-import keyrings.cryptfile.cryptfile as crypt  # type: ignore[import-untyped]
 from jinja2 import BaseLoader, Environment, TemplateError
-from pwinput import pwinput  # type: ignore[import-untyped]
 
-KEYRING_PASS_ENV_VAR = 'EVENTUM_KEYRING_PASSWORD'
-KEYRING_SERVICE_NAME = 'eventum'
+from eventum_cli.keyring import get_secret
 
 
 class TokenValueSource(StrEnum):
     PARAMS = 'params'
     SECRETS = 'secrets'
-
-
-@lru_cache
-def get_keyring_password() -> str:
-    """Get password for keyring from environment variable or prompt it
-    interactively otherwise.
-    """
-    return (
-        os.getenv(KEYRING_PASS_ENV_VAR)
-        or pwinput('Password for keyring: ')
-    )
-
-
-def get_secret(name: str) -> str:
-    """Get secret from keyring for specified `name`. If name is not
-    found in keyring or some error occurs during obtaining data from
-    keyring `ValueError` is raised.
-    """
-
-    keyring = crypt.CryptFileKeyring()
-    keyring.keyring_key = get_keyring_password()
-
-    value = keyring.get_password(
-        service=KEYRING_SERVICE_NAME,
-        username=name
-    )
-
-    if value is None:
-        raise ValueError(
-            f'Name "{name}" not found for service '
-            f'"{KEYRING_SERVICE_NAME}" in keyring'
-        )
-
-    return value
 
 
 def substitute_tokens(content: str, params: dict[str, Any]) -> str:
@@ -69,11 +30,11 @@ def substitute_tokens(content: str, params: dict[str, Any]) -> str:
         if len(parts) != 2:
             raise ValueError(
                 f'Failed to parse token "{token}" in config: '
-                'token name should be in format <source>.<name> where '
+                'token name should be in format <source>.<key> where '
                 f'source can be one of {available_sources}'
             )
 
-        source, name = parts
+        source, key = parts
 
         if source not in available_sources:
             raise ValueError(
@@ -84,18 +45,22 @@ def substitute_tokens(content: str, params: dict[str, Any]) -> str:
 
         match TokenValueSource(source):
             case TokenValueSource.PARAMS:
-                if name not in params:
+                if key not in params:
                     raise ValueError(
-                        f'Parameter "{name}" expected for token "{token}" '
+                        f'Parameter "{key}" expected for token "{token}" '
                         'in config'
                     )
-                value = params[name]
+                value = params[key]
             case TokenValueSource.SECRETS:
-                value = get_secret(name=name)
+                value = get_secret(key=key)
+                if value is None:
+                    raise ValueError(
+                        f'Key "{key}" not found in keyring'
+                    )
             case val:
                 assert_never(val)
 
-        source_to_tokens_map[source][name] = value
+        source_to_tokens_map[source][key] = value
 
     template = Environment(
         loader=BaseLoader(),
