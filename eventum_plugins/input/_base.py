f24e84e46d40f6a124e0b56fcba73dc9a2a37a66
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-08-17 15:45:38 +0300

    Update base input plugin

--- eventum_plugins/input/_base.py
+++ eventum_plugins/input/_base.py
@@ -1,9 +1,6 @@
 from abc import ABC, abstractmethod
-from concurrent.futures import ThreadPoolExecutor
-from typing import Any, Iterator, final
+from typing import Any
 
-from numpy import datetime64
-from numpy.typing import NDArray
 from pydantic import BaseModel
 from pytz.tzinfo import BaseTzInfo
 
@@ -42,55 +39,7 @@ class InputPlugin(ABC):
     def __init__(self, config: Any, tz: BaseTzInfo) -> None:
         ...
 
-
-class LiveInputPluginMixin(ABC):
-    """Input plugin mixin that adds live mode."""
-
-    @final
-    def live(
-        self,
-        batcher: TimestampsBatcher
-    ) -> Iterator[NDArray[datetime64]]:
-        """Start production of event timestamps in live. Iteration
-        starts from only actual timestamps that are not in the past.
-        Iteration is blocked until new events occur in real time.
-        Elements of iteration are arrays of timestamps that batched
-        correspondingly to `batch_size` and `batch_delay`. Iteration
-        may or may not be infinite depending on specific plugin and its
-        configuration.
-        """
-        with ThreadPoolExecutor(max_workers=1) as executor:
-            future = executor.submit(self._live, batcher)
-            yield from batcher.scroll()
-
-            future.result()
-
     @abstractmethod
-    def _live(self, publisher: TimestampsBatcher) -> None:
-        """Internal implementation of `live` mode."""
+    def start(self, batcher: TimestampsBatcher) -> None:
+        """Start generation of timestamps."""
         ...
-
-
-class SampleInputPluginMixin(ABC):
-    """Input plugin mixin that adds sample mode."""
-
-    @final
-    def sample(
-        self,
-        batcher: TimestampsBatcher
-    ) -> Iterator[NDArray[datetime64]]:
-        """Product sample of event timestamps. Iteration is carried out
-        from the first to the last timestamp of generated sample
-        without any delays. Elements of iteration are arrays of
-        timestamps that batched correspondingly to `batch_size`. Number
-        of elements of iteration is always finite.
-        """
-        with ThreadPoolExecutor(max_workers=1) as executor:
-            future = executor.submit(self._sample, batcher)
-            yield from batcher.scroll()
-
-            future.result()
-
-    @abstractmethod
-    def _sample(self, batcher: TimestampsBatcher) -> None:
-        """Internal implementation of `sample` mode."""
