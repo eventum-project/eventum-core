69c2d49b848c4adf191049068f1c04db748afd17
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-08-21 20:39:02 +0300

    Add RelativeTimeString type to fields in input plugins

--- eventum_plugins/input/fields.py
+++ eventum_plugins/input/fields.py
@@ -3,12 +3,25 @@ from typing import Annotated
 import dateparser
 from pydantic import AfterValidator
 
+from eventum_plugins.input.relative_time import parse_relative_time
 
-def _try_parse_datetime(v: str) -> str:
+
+def _try_parse_human_datetime(v: str) -> str:
     if dateparser.parse(v) is None:
         raise ValueError(f'Not valid datetime expression "{v}"')
 
     return v
 
 
-HumanDatetimeString = Annotated[str, AfterValidator(_try_parse_datetime)]
+HumanDatetimeString = Annotated[str, AfterValidator(_try_parse_human_datetime)]
+
+
+def _try_parse_relative_time(v: str) -> str:
+    try:
+        parse_relative_time(v)
+        return v
+    except ValueError:
+        raise ValueError(f'Not valid relative time expression "{v}"')
+
+
+RelativeTimeString = Annotated[str, AfterValidator(_try_parse_relative_time)]
