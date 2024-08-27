ef84dde4e1f49204529b9bcd0c3f0c2ac8330ac4
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-08-27 22:23:49 +0300

    Update normalize_daterange function

--- eventum_plugins/input/tests/test_tools.py
+++ eventum_plugins/input/tests/test_tools.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
 
+import pytest
 import pytz
 
 from eventum_plugins.input.tools import normalize_daterange
 
 
-def test_retrieve_daterange():
+def test_normalize_daterange():
     expected_start = datetime.fromisoformat('2024-01-01T00:00:00.000Z')
     expected_end = datetime(2077, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC'))
 
@@ -19,7 +20,21 @@ def test_retrieve_daterange():
     assert end == expected_end
 
 
-def test_retrieve_daterange_relative_end():
+def test_normalize_daterange_time_keyword():
+    approx_expected_start = datetime.now(tz=pytz.timezone('UTC'))
+    enough_for_me = datetime(2100, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC'))
+
+    start, end = normalize_daterange(
+        start='now',
+        end='never',
+        timezone=pytz.timezone('UTC'),
+    )
+
+    assert 0 < (start - approx_expected_start).total_seconds() < 1
+    assert end > enough_for_me
+
+
+def test_normalize_daterange_human_relative_end():
     expected_start = datetime.fromisoformat('2024-01-01T00:00:00.000Z')
     expected_end = expected_start + timedelta(hours=12, minutes=5)
 
@@ -33,15 +48,62 @@ def test_retrieve_daterange_relative_end():
     assert end == expected_end
 
 
-def test_retrieve_daterange_none_values():
+def test_normalize_daterange_none_values_now_start():
     approx_expected_start = datetime.now(tz=pytz.timezone('UTC'))
     enough_for_me = datetime(2100, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC'))
 
     start, end = normalize_daterange(
         start=None,
         end=None,
-        timezone=pytz.timezone('UTC')
+        timezone=pytz.timezone('UTC'),
+        none_start='now'
     )
 
     assert 0 < (start - approx_expected_start).total_seconds() < 1
     assert end > enough_for_me
+
+
+def test_normalize_daterange_none_values_min_start():
+    enough_early_for_me = datetime(
+        10, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC')
+    )
+    enough_late_for_me = datetime(
+        2100, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC')
+    )
+
+    start, end = normalize_daterange(
+        start=None,
+        end=None,
+        timezone=pytz.timezone('UTC'),
+        none_start='min'
+    )
+
+    assert start < enough_early_for_me
+    assert end > enough_late_for_me
+
+
+def test_normalize_daterange_invalid():
+    with pytest.raises(ValueError):
+        normalize_daterange(
+            start=datetime(
+                2024, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC')
+            ),
+            end=datetime(
+                2014, 1, 1, 0, 0, tzinfo=pytz.timezone('UTC')
+            ),
+            timezone=pytz.timezone('UTC'),
+        )
+
+    with pytest.raises(ValueError):
+        normalize_daterange(
+            start='never',
+            end=None,
+            timezone=pytz.timezone('UTC'),
+        )
+
+    with pytest.raises(ValueError):
+        normalize_daterange(
+            start='qwerty',
+            end=None,
+            timezone=pytz.timezone('UTC'),
+        )
--- eventum_plugins/input/tools.py
+++ eventum_plugins/input/tools.py
@@ -1,83 +1,142 @@
 from datetime import datetime
+from typing import Literal, assert_never
 
 import dateparser
 from pytz import BaseTzInfo
 
 from eventum_plugins.input.fields import (HumanDatetimeString,
-                                          RelativeTimeString)
+                                          RelativeTimeString, TimeKeyword,
+                                          TimeKeywordString)
 from eventum_plugins.input.relative_time import parse_relative_time
 
 
 def normalize_daterange(
-    start: datetime | RelativeTimeString | HumanDatetimeString | None,
-    end: datetime | RelativeTimeString | HumanDatetimeString | None,
-    timezone: BaseTzInfo
+    start: (
+        datetime | TimeKeywordString | RelativeTimeString
+        | HumanDatetimeString | None
+    ),
+    end: (
+        datetime | TimeKeywordString | RelativeTimeString
+        | HumanDatetimeString | None
+    ),
+    timezone: BaseTzInfo,
+    none_start: Literal['now', 'min'] = 'min',
 ) -> tuple[datetime, datetime]:
     """Normalize date range for specified start and end parameters.
-    Parameter values can be of type `datetime`, `str` or `None`. In
-    case of value is `None` in start param - current time is used, in
-    end param - "never" (maximum datetime value) is used. In case of
-    value is of type `str`, it should represent human readable datetime
-    or relative time that can be parsed. Furthermore start datetime is
-    used as relative base for end param expression parsing. In case of
-    value is of type `datetime`, it is only converted to the specified
-    timezone. Types of start and end params can be different from each
-    other within one function call.
 
     Parameters
     ----------
-    start : datetime | RelativeTimeString | HumanDatetimeString | None
-        Start of the date range
+    start : datetime | RelativeTimeString | HumanDatetimeString\
+        | TimeKeywordString | None
+        Start of the date range, used as relative base if `end` is
+        relative expression
 
-    end : datetime | RelativeTimeString | HumanDatetimeString | None
+    end : datetime | RelativeTimeString | HumanDatetimeString\
+        | TimeKeywordString | None
         End of the date range
 
     timezone : BaseTzInfo
-        Timezone that is used for datetime objects of date range
+        Timezone that is used for returned datetime objects
+
+    none_start : Literal['now', 'min']
+        What time to used when `start` parameter is `None`: 'now' -
+        current time; `min` - minimal value of datetime;
 
     Returns
     -------
     tuple[datetime, datetime]
-        Start and end of the daterange as a tuple
+        Normalized start and end of the date range as a tuple in
+        specified timezone
+
+    Raises
+    ------
+    ValueError
+        If provided values cannot be parsed as datetime objects or
+        date range is improper (e.g. start time is later than end time)
     """
+    now = datetime.now(tz=timezone)
+    min = datetime.min.replace(tzinfo=timezone)
+    max = datetime.max.replace(tzinfo=timezone)
+
     match start:
         case datetime():
             start_time = start.astimezone(timezone)
         case str():
             try:
-                timedelta = parse_relative_time(start)
-                start_time = datetime.now(tz=timezone) + timedelta
+                keyword = TimeKeyword(start)
             except ValueError:
-                start_time = dateparser.parse(
-                    start,
-                    settings={                      # type: ignore[arg-type]
-                        'TIMEZONE': timezone.zone,
-                        'RETURN_AS_TIMEZONE_AWARE': True
-                    }
-                )
-                assert isinstance(start_time, datetime)
+                try:
+                    timedelta = parse_relative_time(start)
+                    start_time = now + timedelta
+                except ValueError:
+                    start_time = dateparser.parse(
+                        start,
+                        settings={              # type: ignore[arg-type]
+                            'TIMEZONE': timezone.zone,
+                            'RETURN_AS_TIMEZONE_AWARE': True
+                        }
+                    )
+                    if start_time is None:
+                        raise ValueError(
+                            f'Failed to parse expression "{start}" '
+                            'in parameter "start"'
+                        )
+            else:
+                match keyword:
+                    case TimeKeyword.NOW:
+                        start_time = now
+                    case TimeKeyword.NEVER:
+                        raise ValueError(
+                            'Parameter "start" cannot be "never"'
+                        )
+                    case v:
+                        assert_never(v)
         case _:
-            start_time = datetime.now(tz=timezone)
+            match none_start:
+                case 'min':
+                    start_time = min
+                case 'now':
+                    start_time = now
+                case v:
+                    assert_never(v)
 
     match end:
         case datetime():
             end_time = end.astimezone(timezone)
         case str():
             try:
-                timedelta = parse_relative_time(end)
-                end_time = start_time + timedelta
+                keyword = TimeKeyword(end)
             except ValueError:
-                end_time = dateparser.parse(
-                    end,
-                    settings={                      # type: ignore[arg-type]
-                        'RELATIVE_BASE': start_time,
-                        'TIMEZONE': timezone.zone,
-                        'RETURN_AS_TIMEZONE_AWARE': True
+                try:
+                    timedelta = parse_relative_time(end)
+                    end_time = start_time + timedelta
+                except ValueError:
+                    end_time = dateparser.parse(
+                        end,
+                        settings={                  # type: ignore[arg-type]
+                            'RELATIVE_BASE': start_time,
+                            'TIMEZONE': timezone.zone,
+                            'RETURN_AS_TIMEZONE_AWARE': True
 
-                    }
-                )
-            assert isinstance(end_time, datetime)
+                        }
+                    )
+                if end_time is None:
+                    raise ValueError(
+                        f'Failed to parse expression "{end}" '
+                        'in parameter "end"'
+                    )
+            else:
+                match keyword:
+                    case TimeKeyword.NOW:
+                        end_time = now
+                    case TimeKeyword.NEVER:
+                        end_time = max
+                    case v:
+                        assert_never(v)
         case _:
-            end_time = datetime.max.replace(tzinfo=timezone)
+            end_time = max
+
+    if start_time > end_time:
+        raise ValueError('Start time is later then end time')
 
     return (start_time, end_time)
