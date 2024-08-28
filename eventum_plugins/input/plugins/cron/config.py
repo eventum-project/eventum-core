e17b455b9ca86202133aff4c3463f45b2cd8df85
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-08-28 21:20:56 +0300

    Add config for crontab input plugin

---
+++ eventum_plugins/input/plugins/cron/config.py
@@ -0,0 +1,38 @@
+
+from dataclasses import Field
+
+from croniter import croniter
+from pydantic import field_validator
+
+from eventum_plugins.input.base.config import InputPluginConfig
+from eventum_plugins.input.fields import VersatileDatetime
+
+
+class CronInputPluginConfig(InputPluginConfig, frozen=True):
+    """Configuration for `cron` input plugin.
+
+    Attributes
+    ----------
+    start : VersatileDatetime, default = None
+        Start of the generating date range
+
+    end : VersatileDatetime, default = None
+        End of the generating date range
+
+    expression : str
+        Cron expression
+
+    count : int
+        Number of events to generate for every interval
+    """
+    start: VersatileDatetime = Field(None, union_mode='left_to_right')
+    end: VersatileDatetime = Field(None, union_mode='left_to_right')
+    expression: str
+    count: int = Field(..., gt=0)
+
+    @field_validator('expression')
+    def validate_expression(cls, v: str):
+        if croniter.is_valid(v):
+            return v
+
+        raise ValueError('Invalid cron expression')
