33ec3f4fdc26d6d3b5ab58f7dfb4691ec4f91c47
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-08-30 22:25:52 +0300

    Add mixin DaterangeValidatorMixin for daterange validation

---
+++ eventum_plugins/input/mixins.py
@@ -0,0 +1,21 @@
+from typing import Self
+from pydantic import model_validator
+from pytz import timezone
+
+from eventum_plugins.input.fields import VersatileDatetime
+from eventum_plugins.input.tools import normalize_daterange
+
+
+class DaterangeValidatorMixin:
+    """Mixin for validation date range in plugin configuration models
+    with `start` and `end` fields.
+    """
+
+    @model_validator(mode='after')
+    def validate_interval(self) -> Self:
+        self.start: VersatileDatetime
+        self.end: VersatileDatetime
+
+        normalize_daterange(self.start, self.end, timezone('UTC'))
+
+        return self
