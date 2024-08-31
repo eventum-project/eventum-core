2acb835dcd2b66d9ec584228014f189c66cb9491
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-08-31 12:59:56 +0300

    Add tests for linspace input plugin

---
+++ eventum_plugins/input/plugins/linspace/tests/test_plugin.py
@@ -0,0 +1,77 @@
+from datetime import datetime, timedelta
+
+import pytest
+from numpy import datetime64
+from pytz import timezone
+
+from eventum_plugins.input.enums import TimeMode
+from eventum_plugins.input.plugins.linspace.config import \
+    LinspaceInputPluginConfig
+from eventum_plugins.input.plugins.linspace.plugin import LinspaceInputPlugin
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize(
+    ('start', 'end', 'count', 'endpoint', 'expected'),
+    [
+        ('2024-01-01T00:00:00.000Z', '2024-01-01T00:00:03.000Z', 3, False, [
+            datetime64('2024-01-01T00:00:00.000'),
+            datetime64('2024-01-01T00:00:01.000'),
+            datetime64('2024-01-01T00:00:02.000'),
+        ]),
+        ('2024-01-01T00:00:00.000Z', '2024-01-01T00:00:01.000Z', 5, True, [
+            datetime64('2024-01-01T00:00:00.000'),
+            datetime64('2024-01-01T00:00:00.250'),
+            datetime64('2024-01-01T00:00:00.500'),
+            datetime64('2024-01-01T00:00:00.750'),
+            datetime64('2024-01-01T00:00:01.000'),
+        ]),
+    ]
+)
+def test_linspace_sample(start, end, count, endpoint, expected):
+    config = LinspaceInputPluginConfig(
+        start=datetime.fromisoformat(start),
+        end=datetime.fromisoformat(end),
+        count=count,
+        endpoint=endpoint
+    )
+
+    plugin = LinspaceInputPlugin(
+        config=config,
+        timezone=timezone('UTC'),
+        mode=TimeMode.SAMPLE,
+        id=1
+    )
+
+    events = []
+    for batch in plugin.generate():
+        events.extend(batch)
+
+    assert events == expected
+
+
+@pytest.mark.timeout(1)
+def test_linspace_live():
+    start = datetime.now(tz=timezone('UTC'))
+    end = start + timedelta(microseconds=500)
+    config = LinspaceInputPluginConfig(
+        start=start,
+        end=end,
+        count=100,
+        endpoint=True
+    )
+
+    plugin = LinspaceInputPlugin(
+        config=config,
+        timezone=timezone('UTC'),
+        mode=TimeMode.SAMPLE,
+        id=1
+    )
+
+    events = []
+    for batch in plugin.generate():
+        events.extend(batch)
+
+    assert len(events) <= 100
+    assert events[-1] == datetime64(end.replace(tzinfo=None))
+    assert events[0] >= datetime64(start.replace(tzinfo=None))
