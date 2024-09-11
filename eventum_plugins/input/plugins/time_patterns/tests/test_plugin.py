b09737f1c5efdfef3398c257c9b2b1bcf33a54ba
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-09-11 23:29:21 +0300

    Add tests for time_patterns input plugin

---
+++ eventum_plugins/input/plugins/time_patterns/tests/test_plugin.py
@@ -0,0 +1,88 @@
+import os
+
+import pytest
+from pytz import timezone
+
+from eventum_plugins.exceptions import PluginConfigurationError
+from eventum_plugins.input.enums import TimeMode
+from eventum_plugins.input.plugins.time_patterns.config import \
+    TimePatternsInputPluginConfig
+from eventum_plugins.input.plugins.time_patterns.plugin import \
+    TimePatternsInputPlugin
+
+STATIC_FILES_DIR = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'static'
+)
+
+
+def test_time_pattern_sample():
+    config = TimePatternsInputPluginConfig(
+        configs=[
+            os.path.join(STATIC_FILES_DIR, 'pattern1.yml'),
+            os.path.join(STATIC_FILES_DIR, 'pattern2.yml'),
+            os.path.join(STATIC_FILES_DIR, 'pattern3.yml')
+        ]
+    )
+    plugin = TimePatternsInputPlugin(
+        config=config,
+        id=1,
+        timezone=timezone('UTC'),
+        mode=TimeMode.SAMPLE
+    )
+
+    timestamps = []
+    for batch in plugin.generate():
+        timestamps.extend(batch)
+
+    assert timestamps
+
+    # Uncomment to visualize distribution
+
+    # import matplotlib.pyplot as plt
+    # plt.hist(timestamps, bins=1000)
+    # plt.show()
+
+
+def test_time_pattern_live():
+    config = TimePatternsInputPluginConfig(
+        configs=[
+            os.path.join(STATIC_FILES_DIR, 'pattern1.yml'),
+            os.path.join(STATIC_FILES_DIR, 'pattern2.yml'),
+            os.path.join(STATIC_FILES_DIR, 'pattern3.yml')
+        ]
+    )
+    plugin = TimePatternsInputPlugin(
+        config=config,
+        id=1,
+        timezone=timezone('UTC'),
+        mode=TimeMode.LIVE
+    )
+
+    timestamps = []
+    for batch in plugin.generate():
+        timestamps.extend(batch)
+
+    assert timestamps
+
+    # Uncomment to visualize distribution
+
+    # import matplotlib.pyplot as plt
+    # plt.hist(timestamps, bins=1000)
+    # plt.show()
+
+
+def test_time_pattern_invalid_config():
+    config = TimePatternsInputPluginConfig(
+        configs=[
+            os.path.join(STATIC_FILES_DIR, 'invalid.yml'),
+        ]
+    )
+
+    with pytest.raises(PluginConfigurationError):
+        TimePatternsInputPlugin(
+            config=config,
+            id=1,
+            timezone=timezone('UTC'),
+            mode=TimeMode.LIVE
+        )
