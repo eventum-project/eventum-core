b46dd332937a02d1c1a4072d0d2b1c80f59e38a8
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-09-11 22:23:00 +0300

    Update tests for loader

--- eventum_plugins/tests/test_loader.py
+++ eventum_plugins/tests/test_loader.py
@@ -1,26 +1,28 @@
 from eventum_plugins.enums import PluginType
-from eventum_plugins.input._base import InputPlugin, InputPluginConfig
-from eventum_plugins.loader import (get_event_plugin_names,
-                                    get_input_plugin_names,
-                                    get_output_plugin_names, load_event_plugin,
-                                    load_input_plugin, load_output_plugin)
-
-# from eventum_plugins.event._base import InputPlugin
-# from eventum_plugins.output._base import InputPlugin
+from eventum_plugins.input.base.config import InputPluginConfig
+from eventum_plugins.input.base.plugin import InputPlugin
+from eventum_plugins.loader import get_input_plugin_names, load_input_plugin
 
 
 def test_loading_input_plugin():
-    for plugin_name in get_input_plugin_names():
+    plugin_names = get_input_plugin_names()
+
+    assert plugin_names
+
+    for plugin_name in plugin_names:
         plugin_info = load_input_plugin(plugin_name)
 
+        assert plugin_info.name == plugin_name
+        assert plugin_info.type == PluginType.INPUT
         assert issubclass(plugin_info.cls, InputPlugin)
         assert issubclass(plugin_info.config_cls, InputPluginConfig)
-        assert plugin_info.type == PluginType.INPUT
 
 
-def test_loading_event_plugins():
-    raise NotImplementedError
+# TODO: implement
+# def test_loading_event_plugins():
+#     raise NotImplementedError
 
 
-def test_loading_output_plugins():
-    raise NotImplementedError
+# TODO: implement
+# def test_loading_output_plugins():
+#     raise NotImplementedError
