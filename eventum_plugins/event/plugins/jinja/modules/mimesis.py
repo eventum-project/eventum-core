e9381e1d92a6c610fda35d200489bd055be39b02
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-09-15 22:34:34 +0300

    Optimize faker and mimesis modules for jinja event plugin

--- eventum_plugins/event/plugins/jinja/modules/mimesis.py
+++ eventum_plugins/event/plugins/jinja/modules/mimesis.py
@@ -1,3 +1,5 @@
+from collections.abc import Mapping
+
 import mimesis.enums as enums  # noqa
 import mimesis.random as random  # noqa
 from mimesis import Generic, Locale
@@ -6,49 +8,63 @@ from mimesis.builtins import (BrazilSpecProvider, DenmarkSpecProvider,
                               PolandSpecProvider, RussiaSpecProvider,
                               UkraineSpecProvider, USASpecProvider)
 
-generic = Generic()
 
-brazil_spec = BrazilSpecProvider()
-denmark_spec = DenmarkSpecProvider()
-italy_spec = ItalySpecProvider()
-netherlands_spec = NetherlandsSpecProvider()
-poland_spec = PolandSpecProvider()
-russia_spec = RussiaSpecProvider()
-ukraine_spec = UkraineSpecProvider()
-usa_spec = USASpecProvider()
+class _Locale(Mapping):
+    def __init__(self):
+        self._dict = {}
 
-cs = Generic(Locale.CS)
-da = Generic(Locale.DA)
-de = Generic(Locale.DE)
-de_at = Generic(Locale.DE_AT)
-de_ch = Generic(Locale.DE_CH)
-el = Generic(Locale.EL)
-en = Generic(Locale.EN)
-en_au = Generic(Locale.EN_AU)
-en_ca = Generic(Locale.EN_CA)
-en_gb = Generic(Locale.EN_GB)
-es = Generic(Locale.ES)
-es_mx = Generic(Locale.ES_MX)
-et = Generic(Locale.ET)
-fa = Generic(Locale.FA)
-fi = Generic(Locale.FI)
-fr = Generic(Locale.FR)
-hu = Generic(Locale.HU)
-hr = Generic(Locale.HR)
-is_ = Generic(Locale.IS)
-it = Generic(Locale.IT)
-ja = Generic(Locale.JA)
-kk = Generic(Locale.KK)
-ko = Generic(Locale.KO)
-nl = Generic(Locale.NL)
-nl_be = Generic(Locale.NL_BE)
-no = Generic(Locale.NO)
-pl = Generic(Locale.PL)
-pt = Generic(Locale.PT)
-pt_br = Generic(Locale.PT_BR)
-ru = Generic(Locale.RU)
-sk = Generic(Locale.SK)
-sv = Generic(Locale.SV)
-tr = Generic(Locale.TR)
-uk = Generic(Locale.UK)
-zh = Generic(Locale.ZH)
+    def __getitem__(self, locale: str) -> Generic:
+        if self._dict.__contains__(locale):
+            return self._dict.__getitem__(locale)
+        else:
+            generator = Generic(Locale(locale))
+            self._dict.__setitem__(locale, generator)
+            return generator
+
+    def __iter__(self):
+        return self._dict.__iter__()
+
+    def __len__(self):
+        return self._dict.__len__()
+
+
+class _Spec(Mapping):
+    def __init__(self):
+        self._dict = {}
+
+    def __getitem__(self, spec_name: str) -> Generic:
+        if self._dict.__contains__(spec_name):
+            return self._dict.__getitem__(spec_name)
+        else:
+            match spec_name:
+                case 'brazil_spec':
+                    spec = BrazilSpecProvider()
+                case 'denmark_spec':
+                    spec = DenmarkSpecProvider()
+                case 'italy_spec':
+                    spec = ItalySpecProvider()
+                case 'netherlands_spec':
+                    spec = NetherlandsSpecProvider()
+                case 'poland_spec':
+                    spec = PolandSpecProvider()
+                case 'russia_spec':
+                    spec = RussiaSpecProvider()
+                case 'ukraine_spec':
+                    spec = UkraineSpecProvider()
+                case 'usa_spec':
+                    spec = USASpecProvider()
+                case v:
+                    raise ValueError(f'"{v}" is not valid spec')
+
+            self._dict.__setitem__(spec_name, spec)
+            return spec
+
+    def __iter__(self):
+        return self._dict.__iter__()
+
+    def __len__(self):
+        return self._dict.__len__()
+
+
+locale = _Locale()
+spec = _Spec()
