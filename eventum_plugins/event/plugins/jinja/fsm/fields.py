73b4455677f6ca74d7153e12964af530cf2e3c0a
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-09-28 19:51:57 +0300

    Add new condition checks for FSM mode in jinja event plugin

--- eventum_plugins/event/plugins/jinja/fsm/fields.py
+++ eventum_plugins/event/plugins/jinja/fsm/fields.py
@@ -1,43 +1,134 @@
+from abc import ABC, abstractmethod
 from typing import Any, TypeAlias, Union
 
 from pydantic import BaseModel, Field
 
+from eventum_plugins.event.state import State
 
-class In(BaseModel, frozen=True, extra='forbid'):
-    """Check if value is in sequence."""
-    in_: dict[str, Any] = Field(..., min_length=1, max_length=1, alias='in')
+
+class Checkable(ABC):
+    @abstractmethod
+    def check(self, timestamp: str, tags: list[str], state: State) -> bool:
+        ...
+
+
+SharedStateFieldName: TypeAlias = str
 
 
 class Eq(BaseModel, frozen=True, extra='forbid'):
     """Check if values are equal using '==' operator."""
-    eq: dict[str, Any] = Field(..., min_length=1, max_length=1)
+    eq: dict[SharedStateFieldName, Any] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
 
 
 class Gt(BaseModel, frozen=True, extra='forbid'):
     """Check if value is greater than other value using '>' operator."""
-    gt: dict[str, float | int] = Field(..., min_length=1, max_length=1)
+    gt: dict[SharedStateFieldName, float | int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
 
 
 class Ge(BaseModel, frozen=True, extra='forbid'):
     """Check if value is greater or equal to other value using '>='
     operator.
     """
-    ge: dict[str, float | int] = Field(..., min_length=1, max_length=1)
+    ge: dict[SharedStateFieldName, float | int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
 
 
 class Lt(BaseModel, frozen=True, extra='forbid'):
     """Check if value is lower than other value using '<' operator."""
-    lt: dict[str, float | int] = Field(..., min_length=1, max_length=1)
+    lt: dict[SharedStateFieldName, float | int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
 
 
 class Le(BaseModel, frozen=True, extra='forbid'):
     """Check if value is lower or equal to other value using '<='
     operator.
     """
-    le: dict[str, float | int] = Field(..., min_length=1, max_length=1)
+    le: dict[SharedStateFieldName, float | int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
 
 
-ConditionCheck: TypeAlias = In | Eq | Gt | Ge | Lt | Le
+class LenEq(BaseModel, frozen=True, extra='forbid'):
+    """Check if sequence length is equal to value."""
+    len_eq: dict[SharedStateFieldName, int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
+
+
+class LenGt(BaseModel, frozen=True, extra='forbid'):
+    """Check if sequence length is greater than value."""
+    len_gt: dict[SharedStateFieldName, int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
+
+
+class LenGe(BaseModel, frozen=True, extra='forbid'):
+    """Check if sequence length is greater or equal to value."""
+    len_ge: dict[SharedStateFieldName, int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
+
+
+class LenLt(BaseModel, frozen=True, extra='forbid'):
+    """Check if sequence length is lower than value."""
+    len_lt: dict[SharedStateFieldName, int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
+
+
+class LenLe(BaseModel, frozen=True, extra='forbid'):
+    """Check if sequence length is lower or equal to value."""
+    len_le: dict[SharedStateFieldName, int] = Field(
+        ...,
+        min_length=1,
+        max_length=1
+    )
+
+
+class In(BaseModel, frozen=True, extra='forbid'):
+    """Check if value is in sequence."""
+    in_: dict[SharedStateFieldName, Any] = Field(
+        ...,
+        min_length=1,
+        max_length=1,
+        alias='in'
+    )
+
+
+class HasTags(BaseModel, frozen=True, extra='forbid'):
+    """Check if event has specific tag."""
+    has_tags: str | list[str] = Field(..., min_length=1)
+
+
+ConditionCheck: TypeAlias = (
+    Eq | Gt | Ge | Lt | Le
+    | LenEq | LenGt | LenGe | LenLt | LenLe
+    | In | HasTags
+)
 
 ConditionLogic: TypeAlias = Union['Or', 'And', 'Not']
 
