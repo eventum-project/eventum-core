3cb3710c4ac41029c9ab80d67e6b1d38bb605376
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-10-19 23:53:55 +0300

    Add tests for merger

--- eventum_plugins/input/tests/test_merger.py
+++ eventum_plugins/input/tests/test_merger.py
@@ -19,7 +19,7 @@ def test_merger_with_ordering():
             end='+1s',
             count=100_000,
         ),
-        id='test',
+        id=1,
         live_mode=True,
         timezone=timezone('UTC'),
         batch_size=100,
@@ -61,7 +61,7 @@ def test_merger_with_ordering():
         ordering=True
     )
 
-    batches = list(plugins.generate())
+    batches = list(plugins.generate(include_id=False))
 
     assert all([batch.size <= 1000 for batch in batches])
 
@@ -69,6 +69,72 @@ def test_merger_with_ordering():
 
     assert array.size == 300_000
     assert np.all(array[:-1] <= array[1:])
+
+
+def test_merger_with_ordering_and_ids():
+    start = datetime.now(tz=timezone('UTC'))
+
+    plugin_1 = LinspaceInputPlugin(
+        config=LinspaceInputPluginConfig(
+            start=start + timedelta(seconds=0.5),
+            end='+1s',
+            count=100_000,
+        ),
+        id=1,
+        live_mode=True,
+        timezone=timezone('UTC'),
+        batch_size=100,
+        batch_delay=0.1
+    )
+
+    plugin_2 = LinspaceInputPlugin(
+        config=LinspaceInputPluginConfig(
+            start=start + timedelta(seconds=0.6),
+            end='+1s',
+            count=100_000,
+        ),
+        id=2,
+        live_mode=True,
+        timezone=timezone('UTC'),
+        batch_size=100,
+        batch_delay=0.1
+    )
+
+    plugin_3 = LinspaceInputPlugin(
+        config=LinspaceInputPluginConfig(
+            start=start + timedelta(seconds=0.7),
+            end='+1s',
+            count=100_000,
+        ),
+        id=3,
+        live_mode=True,
+        timezone=timezone('UTC'),
+        batch_size=100,
+        batch_delay=0.1
+    )
+
+    plugins_lst = [plugin_1, plugin_2, plugin_3]
+
+    plugins = InputPluginsLiveMerger(
+        plugins=plugins_lst,
+        target_delay=0.1,
+        batch_size=1000,
+        ordering=True
+    )
+
+    batches = list(plugins.generate(include_id=True))
+
+    assert all([batch.size <= 1000 for batch in batches])
+
+    array = np.concatenate(batches)
+
+    assert array.size == 300_000
+
+    occurrences = dict(zip(*np.unique(array['id'], return_counts=True)))
+
+    assert occurrences[1] == 100_000
+    assert occurrences[2] == 100_000
+    assert occurrences[3] == 100_000
 
 
 def test_merger_without_ordering():
@@ -80,7 +146,7 @@ def test_merger_without_ordering():
             end='+1s',
             count=100_000,
         ),
-        id='test',
+        id=1,
         live_mode=True,
         timezone=timezone('UTC'),
         batch_size=3000,
@@ -122,7 +188,7 @@ def test_merger_without_ordering():
         ordering=False
     )
 
-    batches = list(plugins.generate())
+    batches = list(plugins.generate(include_id=False))
 
     assert all([batch.size <= 1000 for batch in batches])
 
@@ -132,6 +198,72 @@ def test_merger_without_ordering():
     assert not np.all(array[:-1] <= array[1:])
 
 
+def test_merger_without_ordering_with_ids():
+    start = datetime.now(tz=timezone('UTC'))
+
+    plugin_1 = LinspaceInputPlugin(
+        config=LinspaceInputPluginConfig(
+            start=start + timedelta(seconds=0.5),
+            end='+1s',
+            count=100_000,
+        ),
+        id=1,
+        live_mode=True,
+        timezone=timezone('UTC'),
+        batch_size=3000,
+        batch_delay=0.1
+    )
+
+    plugin_2 = LinspaceInputPlugin(
+        config=LinspaceInputPluginConfig(
+            start=start + timedelta(seconds=0.6),
+            end='+1s',
+            count=100_000,
+        ),
+        id=2,
+        live_mode=True,
+        timezone=timezone('UTC'),
+        batch_size=2000,
+        batch_delay=0.1
+    )
+
+    plugin_3 = LinspaceInputPlugin(
+        config=LinspaceInputPluginConfig(
+            start=start + timedelta(seconds=0.7),
+            end='+1s',
+            count=100_000,
+        ),
+        id=3,
+        live_mode=True,
+        timezone=timezone('UTC'),
+        batch_size=1000,
+        batch_delay=0.1
+    )
+
+    plugins_lst = [plugin_1, plugin_2, plugin_3]
+
+    plugins = InputPluginsLiveMerger(
+        plugins=plugins_lst,
+        target_delay=0.1,
+        batch_size=1000,
+        ordering=False
+    )
+
+    batches = list(plugins.generate(include_id=True))
+
+    assert all([batch.size <= 1000 for batch in batches])
+
+    array = np.concatenate(batches)
+
+    assert array.size == 300_000
+
+    occurrences = dict(zip(*np.unique(array['id'], return_counts=True)))
+
+    assert occurrences[1] == 100_000
+    assert occurrences[2] == 100_000
+    assert occurrences[3] == 100_000
+
+
 def test_merger_invalid_params():
     plugin_sample = LinspaceInputPlugin(
         config=LinspaceInputPluginConfig(
@@ -139,7 +271,7 @@ def test_merger_invalid_params():
             end='+1s',
             count=100_000,
         ),
-        id='test',
+        id=1,
         live_mode=False,
         timezone=timezone('UTC'),
         batch_size=100,
@@ -152,7 +284,7 @@ def test_merger_invalid_params():
             end='+1s',
             count=100_000,
         ),
-        id='test',
+        id=2,
         live_mode=True,
         timezone=timezone('UTC'),
         batch_size=100,
