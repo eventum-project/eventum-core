b244a5bd29ef0553854a44847134df503b8bbc76
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-10-20 12:27:25 +0300

    Add possibility to use different types of state in fsm condition fields

---
+++ eventum_plugins/event/plugins/jinja/context.py
@@ -0,0 +1,31 @@
+
+from typing import TypedDict
+
+from eventum_plugins.event.plugins.jinja.state import State
+
+
+class EventContext(TypedDict):
+    """Kwargs for `check` method of  `Checkable`.
+
+    Attributes
+    ----------
+    timestamp : str
+        Timestamp of event
+
+    tags : list[str]
+        Tags from input plugin that generated event
+
+    locals : State
+        Local state of template
+
+    shared : State
+        Shared state of templates
+
+    composed : State
+        Composed state of templates
+    """
+    timestamp: str
+    tags: list[str]
+    locals: State
+    shared: State
+    composed: State
