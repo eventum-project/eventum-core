43702933b28e65a6e004f6acf473b7cddc4f203c
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-10-21 22:24:48 +0300

    Update tests for jinja event plugin state module

--- eventum_plugins/event/plugins/jinja/tests/test_state.py
+++ eventum_plugins/event/plugins/jinja/tests/test_state.py
@@ -42,27 +42,19 @@ def test_single_thread_state_set_get(single_thread_state: SingleThreadState):
     assert single_thread_state.get(key) == value
 
 
+def test_single_thread_state_update(single_thread_state: SingleThreadState):
+    data = {'test_key1': 1, 'test_key2': 2}
+    single_thread_state.update(data)
+    assert single_thread_state.get('test_key1') == 1
+    assert single_thread_state.get('test_key2') == 2
+
+
 def test_single_thread_state_get_default(
     single_thread_state: SingleThreadState
 ):
     key = 'test_key'
     default = 'default_value'
     assert single_thread_state.get(key, default) == default
-
-
-def test_single_thread_state_get_for_update(
-    single_thread_state: SingleThreadState
-):
-    key = 'test_key'
-    value = 'test_value'
-    single_thread_state.set(key, value)
-    assert single_thread_state.get_for_update(key) == value
-
-
-def test_single_thread_state_cancel_update(
-    single_thread_state: SingleThreadState
-):
-    single_thread_state.cancel_update()
 
 
 def test_single_thread_state_as_dict(
@@ -83,6 +75,15 @@ def test_multiprocess_state_set_get(
     assert multiprocess_state.get(key) == value
 
 
+def test_multiprocess_state_update(
+    multiprocess_state: MultiProcessState
+):
+    data = {'test_key1': 1, 'test_key2': 2}
+    multiprocess_state.update(data)
+    assert multiprocess_state.get('test_key1') == 1
+    assert multiprocess_state.get('test_key2') == 2
+
+
 def test_multiprocess_state_get_default(
     multiprocess_state: MultiProcessState
 ):
@@ -98,7 +99,21 @@ def test_multiprocess_state_get_for_update(
     value = 'test_value'
     multiprocess_state.set(key, value)
     assert multiprocess_state.get_for_update(key) == value
+    multiprocess_state.set(key, value + 'updated')
+
+
+def test_multiprocess_state_cancel_update(
+    multiprocess_state: MultiProcessState
+):
+    key = 'test_key'
+    value = 'test_value'
+    multiprocess_state.set(key, value)
+
+    assert multiprocess_state.get_for_update(key) == value
     multiprocess_state.cancel_update()
+
+    with pytest.raises(AssertionError):
+        multiprocess_state.cancel_update()
 
 
 def test_multiprocess_state_as_dict(
@@ -130,7 +145,7 @@ def test_multiprocess_state_concurrent_access(
         max_bytes=1024,
         lock=multiprocess_state_lock
     )
-    processes: list[Process] = []
+    processes = []
     for i in range(5):
         process = Process(target=worker, args=(f'key_{i}', f'value_{i}'))
         processes.append(process)
