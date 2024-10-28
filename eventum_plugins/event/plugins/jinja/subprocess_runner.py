3c936e6bda3c891b45ae0267816e159bae28784d
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-10-28 22:35:07 +0300

    Remove callback setting in SubprocessRunner

--- eventum_plugins/event/plugins/jinja/subprocess_runner.py
+++ eventum_plugins/event/plugins/jinja/subprocess_runner.py
@@ -1,13 +1,8 @@
 import subprocess as subprocess
-from datetime import datetime
-from typing import Any, Callable
 
 
 class SubprocessRunner:
     """Class for running any command in subprocess."""
-
-    def __init__(self) -> None:
-        self._run_callback: Callable[[datetime, str, bool], Any] | None = None
 
     def run(self, command: str, block: bool = False) -> str | None:
         """Start command in a subprocess.
@@ -27,9 +22,6 @@ class SubprocessRunner:
             Stdout of command in case of `block` is `True`, otherwise
             `None`
         """
-        if self._run_callback is not None:
-            self._run_callback(datetime.now().astimezone(), command, block)
-
         proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
         if block:
@@ -37,20 +29,3 @@ class SubprocessRunner:
             return stdout.decode()
 
         return None
-
-    def set_run_callback(
-        self,
-        callback: Callable[[datetime, str, bool], Any] | None
-    ) -> None:
-        """Set callback for `run` method.
-
-        Parameters
-        ----------
-        callback : Callable[[datetime, str, bool], Any] | None
-            Callable or `None` (to unset callback). Callback parameters:
-            1. timestamp of `run` call
-            2. parameter `command` of `run` method
-            3. parameter `block` of `run` method
-
-        """
-        self._run_callback = callback
