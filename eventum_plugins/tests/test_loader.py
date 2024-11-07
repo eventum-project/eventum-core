549e231a2cf1470c55ff252e4fff483c2b04e43c
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-11-07 21:23:31 +0300

    Add loader tests for event plugins

--- eventum_plugins/tests/test_loader.py
+++ eventum_plugins/tests/test_loader.py
@@ -1,7 +1,14 @@
+from pydantic import RootModel
+
+import eventum_plugins.event.plugins as event_plugins
 import eventum_plugins.input.plugins as input_plugins
+from eventum_plugins.event.base.config import EventPluginConfig
+from eventum_plugins.event.base.plugin import EventPlugin
 from eventum_plugins.input.base.config import InputPluginConfig
 from eventum_plugins.input.base.plugin import InputPlugin
-from eventum_plugins.loader import get_input_plugin_names, load_input_plugin
+from eventum_plugins.loader import (get_event_plugin_names,
+                                    get_input_plugin_names, load_event_plugin,
+                                    load_input_plugin)
 
 
 def test_loading_input_plugin():
@@ -15,12 +22,27 @@ def test_loading_input_plugin():
         assert plugin_info.name == plugin_name
         assert plugin_info.package == input_plugins
         assert issubclass(plugin_info.cls, InputPlugin)
-        assert issubclass(plugin_info.config_cls, InputPluginConfig)
+        assert (
+            issubclass(plugin_info.config_cls, InputPluginConfig)
+            or issubclass(plugin_info.config_cls, RootModel)
+        )
 
 
-# TODO: implement
-# def test_loading_event_plugins():
-#     raise NotImplementedError
+def test_loading_event_plugins():
+    plugin_names = get_event_plugin_names()
+
+    assert plugin_names
+
+    for plugin_name in plugin_names:
+        plugin_info = load_event_plugin(plugin_name)
+
+        assert plugin_info.name == plugin_name
+        assert plugin_info.package == event_plugins
+        assert issubclass(plugin_info.cls, EventPlugin)
+        assert (
+            issubclass(plugin_info.config_cls, EventPluginConfig)
+            or issubclass(plugin_info.config_cls, RootModel)
+        )
 
 
 # TODO: implement
