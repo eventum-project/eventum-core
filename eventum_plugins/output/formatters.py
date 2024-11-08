b907ecd71bf81aad270fa864808dd1b24924a537
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-11-08 23:54:12 +0300

    Add output formatters

---
+++ eventum_plugins/output/formatters.py
@@ -0,0 +1,60 @@
+from enum import StrEnum
+import json
+import os
+from typing import assert_never
+
+
+class Format(StrEnum):
+    PLAIN = 'plain'
+    NDJSON = 'ndjson'
+
+
+def format_ndjson(event: str) -> str:
+    """Format event to new line delimited json format.
+
+    Parameters
+    ----------
+    event : str
+        Event to format
+
+    Returns
+    -------
+    str
+        Formatted event
+
+    Raises
+    ------
+    ValueError
+        If formatting failed
+    """
+    try:
+        return json.dumps(json.loads(event), ensure_ascii=False) + os.linesep
+    except json.JSONDecodeError as e:
+        raise ValueError(e) from None
+
+
+def format_event(event: str, format: Format) -> str:
+    """Format event using specified format.
+
+    Parameters
+    ----------
+    format : Format
+        Format to use
+
+    Returns
+    -------
+    str
+        Formatted event
+
+    Raises
+    ------
+    ValueError
+        If formatting failed
+    """
+    match format:
+        case Format.PLAIN:
+            return event
+        case Format.NDJSON:
+            return format_ndjson(event)
+        case f:
+            assert_never(f)
