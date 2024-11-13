cb42127b8aa0d97282a8b8aa51f47febbcdc7791
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-11-13 22:20:24 +0300

    Change base plugin parameter types to generic types, update config class retrieving mechanism

--- eventum_plugins/base/plugin.py
+++ eventum_plugins/base/plugin.py
@@ -2,110 +2,167 @@
 import importlib
 import inspect
 from abc import ABC
-from typing import Any, Required, TypedDict, Unpack
+from types import ModuleType
+from typing import Generic, Required, TypedDict, TypeVar, get_args
 
+from pydantic import RootModel
+
+from eventum_plugins.base.config import PluginConfig
 from eventum_plugins.exceptions import PluginRegistrationError
 from eventum_plugins.registry import PluginInfo, PluginsRegistry
 
 
-class PluginKwargs(TypedDict):
-    """Arguments for plugin configuration.
+class _PluginRegistrationInfo(TypedDict):
+    """Information of plugin for registration.
 
     Attributes
     ----------
-    config : Any
-        Configuration for the plugin
+    name : str
+        Name of the plugin
 
+    package : ModuleType
+        Parent package containing plugin package
+    """
+    name: str
+    package: ModuleType
+
+
+def _inspect_plugin(plugin_cls: type) -> _PluginRegistrationInfo:
+    """Inspect plugin to get registration info.
+
+    Parameters
+    ----------
+    plugin_cls : type
+        Class of the plugin to inspect
+
+    Returns
+    -------
+    _PluginRegistrationInfo
+        Information for plugin registration
+
+    Raises
+    ------
+    TypeError
+        If provided class cannot be inspected
+    """
+    class_module = inspect.getmodule(plugin_cls)
+    if class_module is None:
+        raise TypeError('Cannot get module of plugin class definition')
+
+    if class_module.__name__ == '__main__':
+        raise TypeError('Plugin can be used only as external module')
+
+    try:
+        # expected structure for extraction:
+        # eventum_plugins.<plugin_type>.plugins.<plugin_name>.plugin
+        module_parts = class_module.__name__.split('.')
+        plugin_name = module_parts[-2]
+        plugin_type_package_name = '.'.join(module_parts[:-2])
+    except IndexError:
+        raise TypeError(
+            'Cannot resolve plugin module name or plugin parent package '
+            f'name for module named "{class_module.__name__}"'
+        )
+
+    try:
+        package = importlib.import_module(plugin_type_package_name)
+    except ImportError as e:
+        raise TypeError(
+            'Cannot import parent package of plugin '
+            f'for module named "{class_module.__name__}": {e}'
+        )
+
+    return _PluginRegistrationInfo(name=plugin_name, package=package)
+
+
+class PluginParams(TypedDict):
+    """Parameters for plugin.
+
+    Attributes
+    ----------
     id : int
         Numeric plugin identifier
     """
-    config: Any
     id: Required[int]
 
 
-class Plugin(ABC):
+config_T = TypeVar('config_T', bound=(PluginConfig | RootModel[PluginConfig]))
+params_T = TypeVar('params_T', bound=PluginParams)
+
+
+class Plugin(ABC, Generic[config_T, params_T]):
     """Base class for all plugins.
 
     Parameters
     ----------
-    **kwargs : Unpack[PluginKwargs]
-        Arguments for plugin configuration (see `PluginKwargs`)
+    config : config_T
+        Configuration for the plugin
+
+    params : params_T
+        Parameters for plugin (see `PluginParams`)
 
     Other Parameters
     ----------------
-    config_cls : type
-        Model class of config used by plugin
-
     register : bool, default=True
-        Whether to register class as implemented plugin
+        Whether to register class as complete plugin
 
     Notes
     -----
-    All subclasses of this class is considered as implemented plugins
-    if inheritance parameter `register` is set to `True`. Implemented
+    All subclasses of this class is considered as complete plugins
+    if inheritance parameter `register` is set to `True`. Complete
     plugins are automatically registered in `PluginsRegistry`.
     """
 
-    def __init__(self, **kwargs: Unpack[PluginKwargs]) -> None:
-        self._config = kwargs['config']
-        self._id = kwargs['id']
+    def __init__(self, config: config_T, params: params_T) -> None:
+        self._config = config
+        try:
+            self._id = params['id']
+        except KeyError as e:
+            raise TypeError(f'Missing required parameter: {e}')
 
     def __str__(self) -> str:
-        # plugin_name attribute is set during class initialization
+        # _plugin_name attribute is set during subclass initialization
         return (
             f'{self._plugin_name}-{self._id}'   # type: ignore[attr-defined]
         )
 
-    def __init_subclass__(
-        cls,
-        config_cls: type,
-        register: bool = True,
-        **kwargs
-    ):
+    def __init_subclass__(cls, register: bool = True, **kwargs):
         super().__init_subclass__(**kwargs)
 
         if not register:
             return
 
-        class_module = inspect.getmodule(cls)
-        if class_module is None:
-            raise PluginRegistrationError(
-                'Cannot inspect module of plugin class definition'
-            )
+        try:
+            registration_info = _inspect_plugin(cls)
+        except TypeError as e:
+            raise PluginRegistrationError(f'Unable to inspect plugin: {e}')
 
-        if class_module.__name__ == '__main__':
-            raise PluginRegistrationError(
-                'Plugin can be imported only from external module'
-            )
+        setattr(cls, '_plugin_name', registration_info['name'])
 
         try:
-            # expected structure for plugin name:
-            # eventum_plugins.<plugin_type>.plugins.<plugin_name>.plugin
-            module_parts = class_module.__name__.split('.')
-            plugin_name = module_parts[-2]
-            plugin_type_package_name = '.'.join(module_parts[:-2])
-        except IndexError:
+            config_cls, _ = get_args(
+                cls.__orig_bases__[0]   # type: ignore[attr-defined]
+            )
+        except ValueError:
             raise PluginRegistrationError(
-                'Cannot resolve plugin module name or plugin parent package '
-                f'name for module named "{class_module.__name__}"'
+                'Generic parameters must be specified'
+            )
+        except Exception as e:
+            raise PluginRegistrationError(
+                f'Unable to define config class: {e}'
             )
 
-        try:
-            package = importlib.import_module(plugin_type_package_name)
-        except ImportError as e:
+        if isinstance(config_cls, TypeVar):
             raise PluginRegistrationError(
-                'Cannot import parent package of plugin '
-                f'for module named "{class_module.__name__}": {e}'
+                'Config class cannot have generic type'
             )
-
-        setattr(cls, '_plugin_name', plugin_name)
 
         PluginsRegistry.register_plugin(
             PluginInfo(
-                name=plugin_name,
+                name=registration_info['name'],
                 cls=cls,
                 config_cls=config_cls,
-                package=package
+                package=registration_info['package']
             )
         )
 
