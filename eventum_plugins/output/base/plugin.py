96b1cbc56747adcac46833ca47c4f6c603c1b575
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-11-18 21:50:55 +0300

    Add check wether the plugin is open in base output plugin

--- eventum_plugins/output/base/plugin.py
+++ eventum_plugins/output/base/plugin.py
@@ -4,6 +4,7 @@ from typing import Sequence, TypeVar
 from pydantic import RootModel
 
 from eventum_plugins.base.plugin import Plugin, PluginParams
+from eventum_plugins.exceptions import PluginRuntimeError
 from eventum_plugins.output.base.config import OutputPluginConfig
 
 
@@ -32,20 +33,55 @@ class OutputPlugin(Plugin[config_T, params_T], register=False):
     def __init__(self, config: config_T, params: params_T) -> None:
         super().__init__(config, params)
 
-    @abstractmethod
+        self._is_opened = False
+
     async def open(self) -> None:
+        """Open plugin for writing."""
+        if not self._is_opened:
+            await self._open()
+            self._is_opened = True
+
+    @abstractmethod
+    async def _open(self) -> None:
         """Open plugin for writing."""
         ...
 
-    @abstractmethod
     async def close(self) -> None:
+        """Close plugin for writing with releasing resources and
+        flushing events.
+        """
+        if self._is_opened:
+            await self._close()
+            self._is_opened = False
+
+    @abstractmethod
+    async def _close(self) -> None:
         """Close plugin for writing with releasing resources and
         flushing events.
         """
         ...
 
-    @abstractmethod
     async def write(self, events: Sequence[str]) -> int:
+        """Write events.
+
+        Parameters
+        ----------
+        events : Sequence[str]
+            Sequence of events to write
+
+        Returns
+        -------
+        int
+            Number of successfully written events
+        """
+        if not self._is_opened:
+            raise PluginRuntimeError(
+                'Output plugin is not opened for writing'
+            )
+        return await self._write(events)
+
+    @abstractmethod
+    async def _write(self, events: Sequence[str]) -> int:
         """Write events.
 
         Parameters
