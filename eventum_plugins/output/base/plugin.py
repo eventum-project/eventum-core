d193e4ce07babcf9118fd6998b6fbafcb8f03aad
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-11-18 21:55:53 +0300

    Update docstring in base output plugin

--- eventum_plugins/output/base/plugin.py
+++ eventum_plugins/output/base/plugin.py
@@ -36,15 +36,16 @@ class OutputPlugin(Plugin[config_T, params_T], register=False):
         self._is_opened = False
 
     async def open(self) -> None:
-        """Open plugin for writing."""
+        """Open plugin for writing.
+
+        Raises
+        ------
+        PluginRuntimeError
+            If error occurs during opening
+        """
         if not self._is_opened:
             await self._open()
             self._is_opened = True
-
-    @abstractmethod
-    async def _open(self) -> None:
-        """Open plugin for writing."""
-        ...
 
     async def close(self) -> None:
         """Close plugin for writing with releasing resources and
@@ -53,13 +54,6 @@ class OutputPlugin(Plugin[config_T, params_T], register=False):
         if self._is_opened:
             await self._close()
             self._is_opened = False
-
-    @abstractmethod
-    async def _close(self) -> None:
-        """Close plugin for writing with releasing resources and
-        flushing events.
-        """
-        ...
 
     async def write(self, events: Sequence[str]) -> int:
         """Write events.
@@ -73,12 +67,35 @@ class OutputPlugin(Plugin[config_T, params_T], register=False):
         -------
         int
             Number of successfully written events
+
+        Raises
+        ------
+        PluginRuntimeError
+            If error occurs during writing events
         """
         if not self._is_opened:
             raise PluginRuntimeError(
                 'Output plugin is not opened for writing'
             )
         return await self._write(events)
+
+    @abstractmethod
+    async def _open(self) -> None:
+        """Open plugin for writing.
+
+        Raises
+        ------
+        PluginRuntimeError
+            If error occurs during opening
+        """
+        ...
+
+    @abstractmethod
+    async def _close(self) -> None:
+        """Close plugin for writing with releasing resources and
+        flushing events.
+        """
+        ...
 
     @abstractmethod
     async def _write(self, events: Sequence[str]) -> int:
@@ -93,5 +110,10 @@ class OutputPlugin(Plugin[config_T, params_T], register=False):
         -------
         int
             Number of successfully written events
+
+        Raises
+        ------
+        PluginRuntimeError
+            If error occurs during writing events
         """
         ...
