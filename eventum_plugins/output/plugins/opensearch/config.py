d17d7221ef1b59379bf78377c4f3ab6af73f02ec
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-12-09 11:41:46 +0300

    Add config for opensearch output plugin

---
+++ eventum_plugins/output/plugins/opensearch/config.py
@@ -0,0 +1,55 @@
+
+import os
+
+from pydantic import Field, field_validator
+
+from eventum_plugins.output.base.config import OutputPluginConfig
+
+
+class OpensearchOutputConfig(OutputPluginConfig, frozen=True):
+    """Configuration for `opensearch` output plugin.
+
+    Parameters
+    ----------
+    hosts: list[str]
+        Opensearch cluster nodes that will be used for indexing events,
+        specifying more than one nodes allows for load balancing,
+        nodes must be specified in format `https://<host>:<port>`
+
+    user: str
+        User that is used to authenticate to opensearch for indexing
+        events
+
+    password: str
+        Password for user to authenticate
+
+    index: str
+        Index for writing events
+
+    verify_ssl: bool, default=True
+        Whether to verify SSL certificate of the cluster nodes when
+        connecting to them
+
+    ca_cert_path: str
+        Path to additional CA certificate for SSL verification
+    """
+
+    hosts: list[str] = Field(..., min_length=1)
+    user: str = Field(..., min_length=1)
+    password: str = Field(..., min_length=1)
+    index: str = Field(..., min_length=1)
+    verify_ssl: bool = True
+    ca_cert_path: str | None = None
+
+    @field_validator('ca_cert_path')
+    def validate_ca_cert_path(cls, v: str | None):
+        if not isinstance(v, str):
+            return v
+
+        if not os.path.isabs(v):
+            raise ValueError('Path must be absolute')
+
+        if not os.path.exists(v):
+            raise ValueError(
+                f'Failed to find CA certificate in "{v}"'
+            )
