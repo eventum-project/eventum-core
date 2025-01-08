baaed76c98f2d2a78301b2adce5b43c97839cfb3
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-01-08 22:24:51 +0300

    Refactor cron input plugin

--- eventum/plugins/input/plugins/cron/plugin.py
+++ eventum/plugins/input/plugins/cron/plugin.py
@@ -1,13 +1,11 @@
-import time
 from datetime import datetime
 from typing import Iterator
 
 import croniter
-from numpy import array, datetime64, full, repeat
+from numpy import datetime64, full
+from numpy.typing import NDArray
 
-from eventum.plugins.exceptions import PluginConfigurationError
 from eventum.plugins.input.base.plugin import InputPlugin, InputPluginParams
-from eventum.plugins.input.fields import TimeKeyword
 from eventum.plugins.input.normalizers import normalize_versatile_daterange
 from eventum.plugins.input.plugins.cron.config import CronInputPluginConfig
 
@@ -24,47 +22,10 @@ class CronInputPlugin(InputPlugin[CronInputPluginConfig]):
     ) -> None:
         super().__init__(config, params)
 
-        if (
-            not self._live_mode and (
-                self._config.end is None
-                or self._config.end == TimeKeyword.NEVER.value
-            )
-        ):
-            raise PluginConfigurationError(
-                'End time must be finite for sample mode',
-                context=dict(self.instance_info)
-            )
-
-    def _generate_sample(self) -> None:
-        start, end = normalize_versatile_daterange(
-            start=self._config.start,
-            end=self._config.end,
-            timezone=self._timezone,
-            none_start='now',
-            none_end='max'
-        )
-
-        self._logger.info(
-            'Generating in range',
-            start_timestamp=start.isoformat(),
-            end_timestamp=end.isoformat()
-        )
-
-        range = croniter.croniter_range(
-            start=start.replace(tzinfo=None),
-            stop=end.replace(tzinfo=None),
-            expr_format=self._config.expression,
-            ret_type=datetime
-        )
-
-        timestamps = repeat(
-            a=array(list(range), dtype='datetime64[us]'),
-            repeats=self._config.count
-        )
-
-        self._enqueue(timestamps)
-
-    def _generate_live(self) -> None:
+    def generate(
+        self,
+        skip_past: bool = True
+    ) -> Iterator[NDArray[datetime64]]:
         now = datetime.now().astimezone(self._timezone)
         start, end = normalize_versatile_daterange(
             start=self._config.start,
@@ -80,34 +41,26 @@ class CronInputPlugin(InputPlugin[CronInputPluginConfig]):
             end_timestamp=end.isoformat()
         )
 
-        if end < now:
-            self._logger.info(
-                'All timestamps are in past, nothing to generate'
-            )
-            return
-        elif start < now:
-            start = now
-            self._logger.info('Past timestamps are skipped')
+        if skip_past:
+            if end < now:
+                self._logger.info(
+                    'All timestamps are in past, nothing to generate'
+                )
+                return
+            if start < now:
+                start = now
+                self._logger.info('Past timestamps are skipped')
 
         range: Iterator[datetime] = croniter.croniter_range(
             start=start,
             stop=end,
             expr_format=self._config.expression,
             ret_type=datetime
-
         )
 
         for timestamp in range:
-            now = datetime.now().astimezone(self._timezone)
-            wait_seconds = (timestamp - now).total_seconds()
-
-            if wait_seconds > 0:
-                time.sleep(wait_seconds)
-
-            self._enqueue(
-                full(
-                    shape=self._config.count,
-                    fill_value=datetime64(timestamp.replace(tzinfo=None)),
-                    dtype='datetime64[us]'
-                )
+            yield full(
+                shape=self._config.count,
+                fill_value=datetime64(timestamp.replace(tzinfo=None)),
+                dtype='datetime64[us]'
             )
