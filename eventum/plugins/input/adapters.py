73078e4ffbbb151cd94ec4e96e9105a006b1088e
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-01-14 00:23:13 +0300

    Add TimestampIteratorAdapter to input plugins package

---
+++ eventum/plugins/input/adapters.py
@@ -0,0 +1,39 @@
+from typing import Iterator
+
+import numpy as np
+
+from eventum.plugins.input.base.plugin import InputPlugin
+from eventum.plugins.input.protocols import TimestampIdArray, TimestampIterator
+
+
+class TimestampIteratorAdapter(TimestampIterator):
+    """Adapter for input plugin to follow `TimestampIterator` protocol.
+
+    Parameters
+    ----------
+    plugin : InputPlugin
+        Input plugin to adapt
+    """
+
+    def __init__(self, plugin: InputPlugin) -> None:
+        self._plugin = plugin
+
+    def iterate(
+        self,
+        size: int,
+        skip_past: bool = True
+    ) -> Iterator[TimestampIdArray]:
+        if size < 1:
+            raise ValueError(
+                'Parameter "size" must be greater or equal to 1'
+            )
+
+        for array in self._plugin.generate(size=size, skip_past=skip_past):
+            array_with_id: TimestampIdArray = np.empty(
+                shape=array.size,
+                dtype=[('timestamp', 'datetime64[us]'), ('id', 'uint16')]
+            )
+            array_with_id['timestamp'][:] = array
+            array_with_id['id'][:] = self._plugin.id
+
+            yield array_with_id
---
+++ eventum/plugins/input/tests/test_adapters.py
@@ -0,0 +1,36 @@
+import numpy as np
+from pytz import timezone
+
+from eventum.plugins.input.adapters import TimestampIteratorAdapter
+from eventum.plugins.input.plugins.cron.config import CronInputPluginConfig
+from eventum.plugins.input.plugins.cron.plugin import CronInputPlugin
+
+
+def test_timestamp_iterator_adapter():
+    plugin = CronInputPlugin(
+        config=CronInputPluginConfig(
+            start='now',
+            end='+60s',
+            expression='* * * * * *',
+            count=1
+        ),
+        params={'id': 1437, 'timezone': timezone('UTC')}
+    )
+    adapted = TimestampIteratorAdapter(plugin=plugin)
+
+    plugin_arrays = []
+    for array in plugin.generate(size=1000, skip_past=False):
+        plugin_arrays.append(array)
+
+    adapter_arrays = []
+    for array in adapted.iterate(size=1000, skip_past=False):
+        adapter_arrays.append(array)
+
+    plugin_arr = np.concatenate(plugin_arrays)
+    adapter_arr = np.concatenate(adapter_arrays)
+
+    assert np.array_equal(plugin_arr, adapter_arr['timestamp'])
+
+    ids = set(adapter_arr['id'])
+    assert len(ids) == 1
+    assert ids.pop() == 1437
