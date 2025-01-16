5970355fa651161c8820f456e09a18f90ff9dda5
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-01-16 23:43:25 +0300

    Update generator module

--- eventum/core/generator.py
+++ eventum/core/generator.py
@@ -1,60 +1,31 @@
 from multiprocessing import Process
-from threading import RLock
 
-from eventum.core.parameters.generator import GeneratorParameters
-from eventum.core.runtime.executor import execute
-
-
-class GeneratorError(Exception):
-    """Error during managing generator."""
+from eventum.core.entrypoint import start
+from eventum.core.models.parameters.generator import GeneratorParameters
 
 
 class Generator:
+    """Generator process wrapper."""
+
     def __init__(self, parameters: GeneratorParameters) -> None:
-        self._is_running = False
-        self._lock = RLock()
         self._parameters = parameters
-        self._process = Process(target=execute, args=(parameters, ))
+        self._process = Process(target=start, args=(parameters, ))
 
-    def run(self) -> None:
-        """Run generator with blocking execution until it completes."""
-        with self._lock:
-            if self._is_running:
-                raise GeneratorError('Generator is already running')
+    def start(self) -> None:
+        """Start generator in subprocess."""
+        if self.is_running:
+            return
 
-            self._process.start()
-            self._is_running = True
+        self._process.start()
 
-        self._process.join()
-        self._is_running = False
+    def stop(self) -> None:
+        """Stop generator"""
+        if not self.is_running:
+            return
 
-    def stop(self, timeout: float | None = None) -> None:
-        """Stop generator.
-
-        Parameters
-        ----------
-        timeout : float | None, default=None
-            The number of seconds to wait for the generator if it is
-            not done, if `None` is passed, then there is no limit on
-            the wait time
-
-        Raises
-        ------
-        TimeoutError
-            If timeout expired
-        """
-        with self._lock:
-            if not self._is_running:
-                return
-
-            # TODO: somehow send stop signal
-            self._process.join(timeout)
-            self._is_running = False
-
-    def get_metrics(self) -> GeneratorMetrics:
-        ...
+        self._process.terminate()
 
     @property
     def is_running(self) -> bool:
         """Wether the generator is running."""
-        return self._is_running
+        return self._process.is_alive()
