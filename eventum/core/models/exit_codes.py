d4aa1c331bf7d7b34754f154c3fb82c6b3a9e867
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-01-16 23:45:42 +0300

    Add exit codes enum and function to get description of them

---
+++ eventum/core/models/exit_codes.py
@@ -0,0 +1,32 @@
+from enum import IntEnum
+from typing import assert_never
+
+
+class ExitCode(IntEnum):
+    SUCCESS = 0
+    UNEXPECTED_ERROR = 1
+    CONFIG_ERROR = 2
+
+
+def get_exit_code_description(code: ExitCode) -> str:
+    """Get description of exit code.
+
+    Parameters
+    ----------
+    code : ExitCode
+        Exit code to get description for
+
+    Returns
+    -------
+    str
+        Description of exit code
+    """
+    match code:
+        case ExitCode.SUCCESS:
+            return 'Exited successfully'
+        case ExitCode.UNEXPECTED_ERROR:
+            return 'Unexpected error occurred'
+        case ExitCode.CONFIG_ERROR:
+            return 'Failed to load generator configuration'
+        case v:
+            assert_never(v)
