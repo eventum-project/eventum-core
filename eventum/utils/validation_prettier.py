dbb566adb59646d068e638aba15e7b102eaf049b
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-01-18 22:36:22 +0300

    Move validation_prettier module to utils package

--- eventum/utils/validation_prettier.py
+++ eventum/utils/validation_prettier.py
@@ -3,12 +3,22 @@ from typing import Iterable
 from pydantic_core import ErrorDetails
 
 
-def prettify_errors(errors: list[ErrorDetails]) -> str:
-    """Format pydantic dictionary gotten from `e.errors()` to pretty
-    human readable string."""
+def prettify_validation_errors(errors: Iterable[ErrorDetails]) -> str:
+    """Prettify pydantic validation errors gotten from
+    `e.errors()` to user-friendly description string.
+
+    Parameters
+    ----------
+    errors : Iterable[ErrorDetails]
+        Iterable of error details
+
+    Returns
+    -------
+    str
+        User-friendly description of errors
+    """
 
     def _loc(location: Iterable[str | int]) -> str:
-        """Format field location."""
         return '.'.join(map(str, location))
 
     messages: list[str] = []
@@ -16,7 +26,7 @@ def prettify_errors(errors: list[ErrorDetails]) -> str:
     for error in errors:
         match error:
             case {'type': 'extra_forbidden', 'loc': loc}:
-                msg = 'Field is unrecognized'
+                msg = 'Unknown field'
                 messages.append(f'Field \"{_loc(loc)}\" - {msg}')
             case {'msg': msg, 'loc': loc}:
                 messages.append(f'Field \"{_loc(loc)}\" - {msg}')
