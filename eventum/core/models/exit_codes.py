a9a1a6c0df53cb29de34e18f7259dd1755016856
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-01-22 00:05:48 +0300

    Add new exit code: EXECUTION_ERROR

--- eventum/core/models/exit_codes.py
+++ eventum/core/models/exit_codes.py
@@ -4,9 +4,11 @@ from typing import assert_never
 
 class ExitCode(IntEnum):
     SUCCESS = 0
-    UNEXPECTED_ERROR = 1
-    CONFIG_ERROR = 2
-    PLUGIN_ERROR = 3
+    CONFIG_ERROR = 1
+    PLUGIN_INIT_ERROR = 2
+    EXECUTION_ERROR = 3
+
+    UNEXPECTED_ERROR = 125
 
 
 def get_exit_code_description(code: ExitCode) -> str:
@@ -26,10 +28,12 @@ def get_exit_code_description(code: ExitCode) -> str:
         case ExitCode.SUCCESS:
             return 'Exited successfully'
         case ExitCode.UNEXPECTED_ERROR:
-            return 'Unexpected error occurred'
+            return 'Unexpected error'
         case ExitCode.CONFIG_ERROR:
-            return 'Failed to load generator configuration'
-        case ExitCode.PLUGIN_ERROR:
-            return 'Failed to initialize plugins'
+            return 'Invalid generator configuration'
+        case ExitCode.PLUGIN_INIT_ERROR:
+            return 'Plugin initialization error'
+        case ExitCode.EXECUTION_ERROR:
+            return 'Execution error'
         case v:
             assert_never(v)
